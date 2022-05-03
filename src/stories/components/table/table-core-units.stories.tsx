dfc4f2ce78e8a63ae5b5cbb57e3e667722221a6a
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-05-03 13:47:10 -0300

    Add table to storie book

---
+++ src/stories/components/table/table-core-units.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TableCoreUnits from '../../../components/Table/TableCoreUnits';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Components/Table/Table',
+  component: TableCoreUnits,
+} as ComponentMeta<typeof TableCoreUnits>;
+
+const Template: ComponentStory<typeof TableCoreUnits> = (args) => (
+  <TableCoreUnits />
+);
+
+export const Main = Template.bind({});
+Main.args = {};
