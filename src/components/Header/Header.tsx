192c1c281af6df2660373363b8057c448c9e3df4
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-05-05 11:59:50 -0300

    add-logo-add-css-style-component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,29 @@
 /* eslint-disable */
-import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 
-import AppBar from "@material-ui/core/AppBar";
+import AppBar from '@material-ui/core/AppBar';
 
-import Toggle from "../Svg/Toggle";
-import "./Header.scss";
-import FeedBack from "../Svg/FeedBack";
-import Language from "../Svg/Language";
-import ThemeMode from "../Svg/ThemeMode";
+import Toggle from '../Svg/Toggle';
+import './Header.scss';
+import FeedBack from '../Svg/FeedBack';
+import Language from '../Svg/Language';
+import ThemeMode from '../Svg/ThemeMode';
+import { Typography } from '@mui/material';
+import Logo from '../../stories/components/svg/Logo';
+import styled from '@emotion/styled';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -29,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   hide: {
-    display: "none",
+    display: 'none',
   },
 }));
 
@@ -47,29 +50,30 @@ const Header = ({ open, setOpen }: Props) => {
 
   return (
     <AppBar
-    elevation={0}
-      position="fixed"
+      style={{ background: '#E5E5E5' }}
+      elevation={0}
+      position='fixed'
       className={clsx(classes.appBar, {
         [classes.appBarShift]: open,
       })}
     >
-      <div className="containerHeader">
-        <div className="logoToggleContainer">
-          <div className="logo">
-            <p>SES Logo</p>
-          </div>
-          <div className="toggle">
+      <ContainerHeader>
+        <LogoToggleContainer>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+          <ToggleContainer>
             <Toggle onClick={handleToggleDrawer} />
-          </div>
-        </div>
-        <div className="rightSideHeader">
-          <div className="right">
-            <p>Lorem Ipsum link</p>
-          </div>
-          <div className="right">
-            <p>Lorem Ipsum link</p>
-          </div>
-          <div className="icons">
+          </ToggleContainer>
+        </LogoToggleContainer>
+        <ContainerRight>
+          <Links>
+            <Typography fontSize={16} color='#000000'>Lorem Ipsum link</Typography>
+          </Links>
+          <Links>
+            <Typography fontSize={16} color='#000000'>Lorem Ipsum link</Typography>
+          </Links>
+          <IconContainer>
             <div>
               <FeedBack />
             </div>
@@ -79,11 +83,71 @@ const Header = ({ open, setOpen }: Props) => {
             <div>
               <ThemeMode />
             </div>
-          </div>
-        </div>
-      </div>
+          </IconContainer>
+        </ContainerRight>
+      </ContainerHeader>
     </AppBar>
   );
 };
+
+const ContainerHeader = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: '64px',
+  backgroundColor: '#c4c4c4;'
+})
+
+const LogoToggleContainer = styled.div({
+  height: '64px',
+  width: '260px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  backgroundColor: '#b6b6b6'
+})
+
+const LogoContainer = styled.div({
+  height: '32px',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'row',
+  alignItems: 'center',
+  backgroundColor: '#b6b6b6',
+  marginLeft: '32px',
+})
+
+const ToggleContainer = styled.div({
+  marginRight: '32px',
+})
+
+const ContainerRight = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+})
+
+const Links = styled.div({
+  width: '151px',
+  height: '27px',
+  backgroundColor: '#ededed',
+  borderRadius: '5px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginRight: '16px',
+})
+
+
+const IconContainer = styled.div({
+  minWidth: '160px',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginRight: '34px',
+  padding: '5px'
+})
 
 export default Header;
