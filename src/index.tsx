bc4a6dfc6cf9d5eddadb25c38d57e2cffb14325b
Maykel Lopez <maykel.meneses89@gmail.com>
2022-05-10 19:55:18 +0200

    [SES-31] Lint Fix

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import {
   gql
 } from '@apollo/client';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
