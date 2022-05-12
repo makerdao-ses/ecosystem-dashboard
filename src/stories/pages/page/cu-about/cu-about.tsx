813322f3aae91f8574b68349eb1c6bfd495d56ca
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-05-12 14:04:22 -0300

    pass-correct-props-cu-about

--- src/stories/pages/page/cu-about/cu-about.tsx
+++ src/stories/pages/page/cu-about/cu-about.tsx
@@ -13,7 +13,9 @@ import BigButton from '../../../components/button/big-button/big-button';
 import { CuStatusEnum } from '../../../../core/enums/cu-status-enum';
 
 const CuAbout = () => {
-  const markdownText = 'https://raw.githubusercontent.com/mact200590/Proyectos/master/example.md';
+  const sentenceDescription = 'SES aims to sustainably grow the Maker Protocol’s moats by removing barriers between decentralized workforce, capital, and work.';
+  const paragraphDescription = '**Vision**\n    \n    An effective, decentralized, and scalable ecosystem:\n    \n    1. The industry’s best on-boarding experience and retention rate\n    2. Easy to find the capital needed for the best projects to work on:\n        1. Optimal driving force for protocol growth\n        2. Most fulfilling for the project’s participants\n    3. Resilient safety mechanisms\n        1. Preventing protocol failure\n        2. Allow for rapid innovation and experimentation\n \n **Strategy**\n \n    - Opportunity & Risk Assessment\n        - Engage with the different DAO stakeholders and keep an open backlog accessible to the broader Community.\n        - Prioritize issues based on importance and risk and build a Road Map to tackle these issues.\n    - Research\n        - Research the opportunities and issues, with a data-centric approach.\n        - Explore solutions and frameworks that produce high-quality, repeatable results.\n    - Incubate\n        - Set groups for success into becoming a functional Core Unit answering a specific need for the DAO. Guide and support them through the process.\n        - Feedback the research through continuous improvement to accelerate the scale and improve the success rate of new Core Units.';
+  const paragraphImage = 'https://gateway-proxy-bee-9-0.gateway.ethswarm.org/bzz/1fe299c01206d1d422cf79a60ea49b8a77b04382f8d25745842eb2a199eb4389';
   const handleClickPrevious = useCallback(
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     () => {
@@ -123,7 +125,7 @@ const CuAbout = () => {
         <TitleNavigationCuAbout status={CuStatusEnum.Accepted} title='Sustainable Ecosystem Scaling' statusModified={new Date()} links={links} />
       </ContainerTitle>
       <MarkdownContainer>
-        <MdViewerContainer markdown={markdownText} />
+        <MdViewerContainer paragraphDescription={paragraphDescription} paragraphImage={paragraphImage} sentenceDescription={sentenceDescription} />
       </MarkdownContainer>
       <TeamMemberContainer>
         <TeamMemberTitle>Team Size</TeamMemberTitle><TeamMember fte={7.5} />
