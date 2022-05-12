ceef6471d0bd771f57970def1b07b7c186a7bf45
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-05-12 13:54:01 -0300

    SES-correct-component-Api-Data

--- src/stories/components/markdown/md-view-container.tsx
+++ src/stories/components/markdown/md-view-container.tsx
@@ -5,10 +5,12 @@ import MdViewerPage, { MarkDownHeaders } from './md-view';
 import makerRender from './renderUtils';
 
 interface Props {
-  markdown: string;
+  sentenceDescription: string;
+  paragraphDescription: string;
+  paragraphImage: string;
 }
 
-const MdViewerContainer = ({ markdown }: Props) => {
+const MdViewerContainer = ({ sentenceDescription, paragraphDescription, paragraphImage }: Props) => {
   const [headersLevel, setHeadersLevel] = useState<MarkDownHeaders[]>([]);
   const creatingIndexItems = useCallback(
     (level: number, htmlCleanedText: string, escapedText: string) => {
@@ -29,13 +31,19 @@ const MdViewerContainer = ({ markdown }: Props) => {
     },
     [headersLevel],
   );
-  const parsed = marked.parse(markdown);
+
+  const convertImg = `![Image](${paragraphImage})`;
+  const sentence = marked.parse(sentenceDescription);
+  const paragraph = marked.parse(paragraphDescription);
+  const image = marked.parse(convertImg);
   marked.use({
     renderer: makerRender({ forEachHeading: creatingIndexItems }),
   });
   return (
     <MdViewerPage
-      markdownText={parsed}
+      paragraphImage={image}
+      sentenceDescription={sentence}
+      paragraphDescription={paragraph}
       headersLevel={headersLevel}
     />
   );
