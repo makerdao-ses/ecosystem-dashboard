d8d81e407a011c377d07d096f82d26f3b733fc0a
Maykel Lopez <maykel.meneses89@gmail.com>
2022-05-24 12:13:23 +0200

    [SES-48] Checking lint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import { DashboardWrapper } from './stories/containers/dashboard-wrapper/dashboard-wrapper';
+import { DashboardWrapper } from "./stories/containers/dashboard-wrapper/dashboard-wrapper";
 import { CuTable } from './stories/containers/cu-table/cu-table';
-import { HOME_ROUTE, ABOUT_ROUTE } from './config/routes';
+import { HOME_ROUTE, ABOUT_ROUTE } from './config/routes'
 import CuAbout from './stories/pages/page/cu-about/cu-about';
 
 function App() {
