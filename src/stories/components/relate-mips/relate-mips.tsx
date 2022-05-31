4a6504ecb8a625a34d4fee8d86e26f0da73410ab
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-05-31 18:22:58 -0300

    update-relate-mips-only-links-and-icon

--- src/stories/components/relate-mips/relate-mips.tsx
+++ src/stories/components/relate-mips/relate-mips.tsx
@@ -3,10 +3,11 @@ import styled from '@emotion/styled';
 import { Typography } from '@mui/material';
 import { DateTime } from 'luxon';
 import { CustomPopover } from '../custom-popover/custom-popover';
-import ArrowLink from '../svg/ArrowLink';
 import { CuStatusEnum } from '../../../core/enums/cu-status.enum';
 import { StatusChip } from '../status-chip/status-chip';
 import { CuMip } from '../../containers/cu-about/cu-about.api';
+import ExternalLinkArrow from '../svg/external-link-arrow';
+import { getMipTitle } from '../../../core/utils/string.utils';
 
 export type RelateMipType = {
   status: CuStatusEnum,
@@ -39,6 +40,8 @@ const RelateMips = ({ relateMips }: Props) => {
   const mips = getMipsStatus(relateMips || '');
   const mipStatus = relateMips.mipStatus;
   const newDate = mips ? DateTime.fromFormat(mips || '', 'yyyy-MM-dd').toJSDate() : null;
+  const pieces = getMipTitle(relateMips.mipTitle);
+  console.log('pieces', pieces);
   return (
     <Content>
       <Row>
@@ -54,11 +57,22 @@ const RelateMips = ({ relateMips }: Props) => {
           </SinceDate>
         </CustomPopover>}
       </Row>
-      <RowUnderLine>
-        <Typography color='#000000' fontSize={12} fontWeight={600} sx={{ marginRight: '19px' }}>{relateMips.mipTitle}
-          <ArrowLinkContainer><ArrowLink href={`${relateMips.mipUrl}` || '#'} target="_blank" /></ArrowLinkContainer>
-        </Typography>
-      </RowUnderLine>
+      {pieces.length === 2 && <RowUnderLine>
+        <Typography color='#231536' fontSize={16} fontWeight={600} sx={{
+          marginRight: '4px',
+          lineHeight: '16px',
+          minWidth: '120px',
+          display: 'inline-block',
+        }}>{`${pieces[0]} :`}</Typography>
+        <ContainerIconTypography>
+        <Typography color='#447AFB' fontSize={16} sx={{
+          display: 'inline',
+          lineHeight: '19px',
+        }}>{pieces[1]}</Typography>
+        <ArrowLinkContainer>  <ExternalLinkArrow href={`${relateMips.mipUrl}` || '#'} /></ArrowLinkContainer>
+        </ContainerIconTypography>
+      </RowUnderLine>}
+      {pieces.length === 1 && <RowUnderLine>{relateMips.mipTitle}</RowUnderLine>}
     </Content>
   );
 };
@@ -67,7 +81,7 @@ const Content = styled.div({
   display: 'flex',
   flexDirection: 'column',
   padding: '8px 4px 17px 8px',
-  width: '500px',
+  width: '672px',
   backgroundColor: '#FCFCFC',
   boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.25)',
   borderRadius: '8px',
@@ -81,11 +95,11 @@ const Row = styled.div({
 });
 
 const RowUnderLine = styled.div({
-  display: 'inline-block',
-  alignItems: 'center',
-  verticalAlign: 'middle',
-  textDecoration: 'underline',
-  whiteSpace: 'pre-wrap',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'flex-start',
+  wordWrap: 'normal',
+  paddingBottom: '16px'
 });
 
 const SinceDate = styled.a({
@@ -98,7 +112,10 @@ const SinceDate = styled.a({
 });
 
 const ArrowLinkContainer = styled.span({
+  display: 'inline',
   marginLeft: '9px',
+});
+const ContainerIconTypography = styled.div({
+  display: 'inline',
   alignItems: 'center',
-  verticalAlign: 'middle',
 });
