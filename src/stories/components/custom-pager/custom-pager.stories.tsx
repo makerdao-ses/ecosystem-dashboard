e2fc7672a774ab54202b39e3d44569f4a7977c5a
Maykel Lopez <maykel.meneses89@gmail.com>
2022-06-07 13:37:15 +0200

    [SES-51] Created pager component

---
+++ src/stories/components/custom-pager/custom-pager.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { CustomPager } from './custom-pager';
+
+export default {
+  title: 'Components/General/CustomPager',
+  component: CustomPager
+} as ComponentMeta<typeof CustomPager>;
+
+const Template: ComponentStory<typeof CustomPager> = (args) => <CustomPager {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Custom button'
+};
---
+++ src/stories/components/custom-pager/custom-pager.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { ChevronLeft } from '../svg/chevron-left';
+import { ChevronRight } from '../svg/chevron-right';
+
+interface CustomPagerProps {
+  label: string,
+  onNext?: () => void,
+  onPrev?: () => void,
+}
+
+export const CustomPager = (props: CustomPagerProps) => {
+  return <Container className="no-select">
+    <Label>{props.label}</Label>
+    <Arrows>
+      <IconWrapper onClick={props.onPrev}>
+        <ChevronLeft/>
+      </IconWrapper>
+      <IconWrapper onClick={props.onNext}>
+        <ChevronRight/>
+      </IconWrapper>
+    </Arrows>
+  </Container>;
+};
+
+const Container = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const Label = styled.div({
+  fontFamily: 'FT Base, sans-serif',
+  fontWeight: 500,
+  fontSize: '20px',
+  lineHeight: '24px',
+  letterSpacing: '0.4px',
+  color: '#9FAFB9',
+  marginRight: '16px',
+});
+
+const Arrows = styled.div({
+  display: 'flex'
+});
+
+const IconWrapper = styled.div({
+  marginRight: '8px',
+  cursor: 'pointer',
+});
