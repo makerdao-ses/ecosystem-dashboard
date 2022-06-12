8699c196fe9930546633386ebed5363934bf803f
Maykel Lopez <maykel.meneses89@gmail.com>
2022-06-12 12:06:23 +0200

    [SES-51] Added category chip

---
+++ src/stories/components/category-chip/category-chip.tsx
@@ -0,0 +1,61 @@
+import React, { CSSProperties } from 'react';
+import styled from '@emotion/styled';
+import { CuCategoryEnum } from '../../../core/enums/cu-category.enum';
+
+interface StatusChipProps {
+  category: CuCategoryEnum;
+  style?: CSSProperties;
+}
+
+const colors = {
+  Technical: {
+    color: '#546978',
+    background: 'rgba(246, 245, 255, 0.5)'
+  },
+  Support: {
+    color: '#1AAB9B',
+    background: 'rgba(245, 255, 246, 0.5)'
+  },
+  Operational: {
+    color: '#9055AF',
+    background: 'rgba(250, 245, 255, 0.5)'
+  },
+  Business: {
+    color: '#F08B04',
+    background: 'rgba(255, 251, 245, 0.5)'
+  },
+  RWAs: {
+    color: '#2DC1B1',
+    background: 'rgba(245, 255, 249, 0.5)'
+  },
+  Growth: {
+    color: '#DC5D00',
+    background: 'rgba(255, 245, 245, 0.5)'
+  },
+  Finances: {
+    color: '#447AFB',
+    background: 'rgba(247, 255, 245, 0.52)'
+  }
+};
+
+export const CategoryChip = (props: StatusChipProps) => {
+  return <Chip style={{
+    color: colors[props.category].color,
+    borderColor: colors[props.category].color,
+    background: colors[props.category].background,
+    ...props.style
+  }}>{props.category}</Chip>;
+};
+
+const Chip = styled.div({
+  fontFamily: 'FT Base, sans-serif',
+  display: 'flex',
+  alignItems: 'center',
+  fontWeight: 400,
+  fontSize: '11px',
+  borderRadius: '12px',
+  padding: '0 8px',
+  height: '22px',
+  width: 'fit-content',
+  border: '1px solid black',
+});
