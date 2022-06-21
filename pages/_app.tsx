5e233797ab9737958a7511f8016b07c38aaaca18
Maykel Lopez <maykel.meneses89@gmail.com>
2022-06-21 16:39:00 +0200

    [SES-62] Added favicon and page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import { store } from '../src/core/store/store';
 import { HeaderWrapper } from '../src/stories/containers/dashboard-wrapper/header-wrapper';
+import Head from 'next/head';
 
 function MyApp({
   Component, pageProps,
@@ -10,6 +11,11 @@ function MyApp({
   return (
     <Provider store={store}>
       <HeaderWrapper>
+        <Head>
+          <title>MakerDao - Dashboard</title>
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
+
         <Component {...pageProps} />
       </HeaderWrapper>
     </Provider>
