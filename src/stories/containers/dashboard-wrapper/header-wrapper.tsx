78b1e60411a4b75821a652c545186b455bcca772
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-06-28 14:17:20 -0300

    add-example-header-dark-mode

--- src/stories/containers/dashboard-wrapper/header-wrapper.tsx
+++ src/stories/containers/dashboard-wrapper/header-wrapper.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import mainTheme from '../../../core/styling/main-theme';
-import Header from '../../components/header/Header';
-import { itemsWebSiteLinks } from '../../components/header/select-link-website/menu-items';
+
 import styled from '@emotion/styled';
-import menuItems from '../../components/header/menu-items';
+import { ThemeMode } from '../../../core/context/ThemeContext';
 
 interface HeaderWrapperProps {
-  children?: JSX.Element | JSX.Element[]
+  children?: JSX.Element | JSX.Element[];
+  themeMode: ThemeMode
+  toggleTheme: () => void
 }
-export const HeaderWrapper = (props: HeaderWrapperProps) => {
+export const HeaderWrapper = ({ children }: HeaderWrapperProps) => {
   return (
-    <ThemeProvider theme={mainTheme}>
-      <Header menuItems={menuItems} links={itemsWebSiteLinks} />
-      <Container>
-        <CssBaseline />
-        {props.children}
-      </Container>
-    </ThemeProvider>
+    < Container >
+      <CssBaseline />
+      {children}
+    </Container >
   );
 };
 
