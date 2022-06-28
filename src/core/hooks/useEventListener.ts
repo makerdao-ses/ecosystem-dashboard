dd23244fb7c717a7f54748e6207d519e0fbf32ca
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-06-28 14:13:37 -0300

    Add hooks for persisnte and user preferencies

---
+++ src/core/hooks/useEventListener.ts
@@ -0,0 +1,36 @@
+// source: https://usehooks-ts.com/react-hook/use-event-listener
+import { RefObject, useEffect, useRef } from 'react';
+
+function useEventListener<T extends HTMLElement = HTMLDivElement>(
+  eventName: keyof WindowEventMap | string, // string to allow custom event
+  handler: (event: Event) => void,
+  element?: RefObject<T>
+) {
+  const savedHandler = useRef<(event: Event) => void>();
+
+  useEffect(() => {
+    const targetElement: T | Window = element?.current || window;
+    if (!(targetElement && targetElement.addEventListener)) {
+      return;
+    }
+
+    if (savedHandler.current !== handler) {
+      savedHandler.current = handler;
+    }
+
+    const eventListener = (event: Event) => {
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!!savedHandler?.current) {
+        savedHandler.current(event);
+      }
+    };
+
+    targetElement.addEventListener(eventName, eventListener);
+
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element, handler]);
+}
+
+export default useEventListener;
