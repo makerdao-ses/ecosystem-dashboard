36c65aff556fddd8d55b63c6acf71c7ace89fd27
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-06-28 14:19:24 -0300

    Move Header outside wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { ReactElement, ReactNode, useCallback, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import { store } from '../src/core/store/store';
 import { HeaderWrapper } from '../src/stories/containers/dashboard-wrapper/header-wrapper';
 import Head from 'next/head';
 import '../styles/globals.scss';
+import createEmotionCache from '../src/core/utils/emotion-cache';
+import { NextPage } from 'next';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, useThemeContext } from '../src/core/context/ThemeContext';
 
-function MyApp({
-  Component, pageProps,
-}: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+interface MyAppProps extends AppProps {
+  Component: NextPageWithLayout
+  emotionCache?: EmotionCache
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { themeMode, toggleTheme } = useThemeContext();
+
   return (
-    <Provider store={store}>
-      <HeaderWrapper>
-        <Head>
-          <title>MakerDao - Dashboard</title>
-          <link rel="icon" href="/favicon.svg" />
-        </Head>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider>
+        <Provider store={store}>
 
-        <Component {...pageProps} />
-      </HeaderWrapper>
-    </Provider>
+          <Head>
+            <title>MakerDao - Dashboard</title>
+            <link rel="icon" href="/favicon.svg" />
+          </Head>
+          <HeaderWrapper themeMode={themeMode} toggleTheme={toggleTheme}
+          >
+            <Component {...pageProps} />
+          </HeaderWrapper>
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
