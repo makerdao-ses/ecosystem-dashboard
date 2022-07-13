20392bf0b9f325b4b378aac2c78b847cd8351e38
Maykel Lopez <maykel.meneses89@gmail.com>
2022-07-13 10:31:39 +0200

    [SES-52] Added FTEs to mkr vesting

--- src/stories/containers/transparency-report/transparency-mkr-vesting/transparency-mkr-vesting.mvvm.ts
+++ src/stories/containers/transparency-report/transparency-mkr-vesting/transparency-mkr-vesting.mvvm.ts
@@ -5,27 +5,37 @@ import { API_MONTH_FORMAT } from '../../../../core/utils/date.utils';
 import _ from 'lodash';
 
 export const useTransparencyMkrVesting = (currentMonth: DateTime, budgetStatements: BudgetStatementDto[]) => {
+  const currentBudgetStatement = useMemo(() => {
+    return budgetStatements.find(bs => bs.month === currentMonth.toFormat(API_MONTH_FORMAT));
+  }, [currentMonth, budgetStatements]);
+
   const mkrVestings = useMemo(() => {
     if (!currentMonth || !budgetStatements || !budgetStatements.length) return [];
 
-    return budgetStatements.find(bs => bs.month === currentMonth.toFormat(API_MONTH_FORMAT))?.budgetStatementMKRVest ?? [];
+    return currentBudgetStatement?.budgetStatementMKRVest ?? [];
   }, [currentMonth, budgetStatements]);
 
   const totalAmount = useMemo(() => {
     if (!currentMonth || !budgetStatements || !budgetStatements.length) return [];
 
-    return _.sumBy(budgetStatements.find(bs => bs.month === currentMonth.toFormat(API_MONTH_FORMAT))?.budgetStatementMKRVest ?? [], mkr => mkr.mkrAmount);
+    return _.sumBy(currentBudgetStatement?.budgetStatementMKRVest ?? [], mkr => mkr.mkrAmount);
   }, [currentMonth, budgetStatements]);
 
   const totalOldAmount = useMemo(() => {
     if (!currentMonth || !budgetStatements || !budgetStatements.length) return [];
 
-    return _.sumBy(budgetStatements.find(bs => bs.month === currentMonth.toFormat(API_MONTH_FORMAT))?.budgetStatementMKRVest ?? [], mkr => mkr.mkrAmountOld);
+    return _.sumBy(currentBudgetStatement?.budgetStatementMKRVest ?? [], mkr => mkr.mkrAmountOld);
+  }, [currentMonth, budgetStatements]);
+
+  const FTEs = useMemo(() => {
+    console.log(currentBudgetStatement);
+    return _.first(currentBudgetStatement?.budgetStatementFTEs)?.ftes ?? 0;
   }, [currentMonth, budgetStatements]);
 
   return {
     mkrVestings,
     totalAmount,
-    totalOldAmount
+    totalOldAmount,
+    FTEs
   };
 };
