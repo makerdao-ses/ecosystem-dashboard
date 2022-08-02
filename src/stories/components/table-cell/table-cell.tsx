679d5dbb9512ce3d5efb81751cb5d56277cf8a6f
Maykel Lopez <maykel.meneses89@gmail.com>
2022-08-02 20:52:40 +0200

    [SES-91] Added transfer request responsive

--- src/stories/components/table-cell/table-cell.tsx
+++ src/stories/components/table-cell/table-cell.tsx
@@ -6,19 +6,23 @@ interface TableCellProps {
   children?: string | JSX.Element | JSX.Element[];
   style?: CSSProperties;
   fontFamily?: string;
+  responsivePadding?: string;
 }
 
 export const TableCell = (props: TableCellProps) => {
-  return <Container fontFamily={props.fontFamily} style={props.style} negative={props.negative}>{props.children}</Container>;
+  return <Container fontFamily={props.fontFamily} style={props.style} negative={props.negative} responsivePadding={props.responsivePadding}>{props.children}</Container>;
 };
 
-const Container = styled.div<{ negative?: boolean, fontFamily?: string}>(({ negative = false, fontFamily = 'FT Base, sans-serif' }) => ({
+const Container = styled.div<{ negative?: boolean, fontFamily?: string, responsivePadding?: string}>(({ negative = false, fontFamily = 'FT Base, sans-serif', responsivePadding }) => ({
   fontFamily,
   fontWeight: 400,
   fontSize: 16,
-  padding: '16px',
+  padding: responsivePadding || '16px',
   color: negative ? '#F75524' : '#231536',
   '> b': {
     fontWeight: 700,
+  },
+  '@media (min-width: 825px)': {
+    padding: '16px',
   }
 }));
