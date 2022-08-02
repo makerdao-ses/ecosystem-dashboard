bc7feb790419412961c1dc43ae67d3ee1d365083
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-08-02 12:56:46 -0300

    Add dark mode BradCrumbs component

--- src/stories/components/breadcrumbs/breadcrumbs.tsx
+++ src/stories/components/breadcrumbs/breadcrumbs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { BreadcrumbSeparator } from '../svg/breadcrumb-separator';
 import Link from 'next/link';
+import { useThemeContext } from '../../../core/context/ThemeContext';
 
 interface BreadcrumbsProps {
   items: {
@@ -11,14 +12,15 @@ interface BreadcrumbsProps {
 }
 
 export const Breadcrumbs = (props: BreadcrumbsProps) => {
+  const isLight = useThemeContext().themeMode === 'light';
   return <Container>
     {props.items.map((item, i) => <>
       <Link href={item.url} style={{
         pointerEvents: item.url && !(i === props.items.length - 1) ? 'all' : 'none',
       }}>
-        <Crumb last={i === props.items.length - 1} first={i === 0}>{item.label}</Crumb>
+        <Crumb isLight={isLight} last={i === props.items.length - 1} first={i === 0}>{item.label}</Crumb>
       </Link>
-      {i !== props.items.length - 1 && <BreadcrumbSeparator/>}
+      {i !== props.items.length - 1 && <BreadcrumbSeparator fillDark='#787A9B' fill='#D1DEE6' />}
     </>)}
   </Container>;
 };
@@ -32,15 +34,15 @@ const Container = styled.div({
   alignSelf: 'flex-start',
 });
 
-const Crumb = styled.a((props: { first: boolean, last: boolean }) => ({
+const Crumb = styled.a<{ first: boolean, last: boolean, isLight: boolean }>(({ first, last, isLight }) => ({
   fontFamily: 'FT Base, sans-serif',
-  fontWeight: props.last ? 500 : 400,
+  fontWeight: last ? 500 : 400,
   fontSize: '16px',
   lineHeight: '19px',
   textAlign: 'center',
   letterSpacing: '0.4px',
-  color: props.last ? '#231536' : '#708390',
+  color: last && isLight ? '#231536' : !last && isLight ? '#708390' : last && !isLight ? '#D2D4EF' : '#787A9B',
   marginRight: '15px',
-  marginLeft: props.first ? '0' : '15px',
+  marginLeft: first ? '0' : '15px',
   cursor: 'pointer',
 }));
