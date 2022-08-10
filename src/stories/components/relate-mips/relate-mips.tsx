1310921f10088076f58b13007190ad3fe5689b69
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-08-10 16:31:42 -0300

    fix-pop-over-relate-mips

--- src/stories/components/relate-mips/relate-mips.tsx
+++ src/stories/components/relate-mips/relate-mips.tsx
@@ -40,6 +40,11 @@ const RelateMips = ({ relateMips }: Props) => {
             flexDirection: 'column',
             alignItems: 'center'
           }}
+          popupStyle={{
+            background: isLight ? 'white' : '#000A13',
+            color: isLight ? '#231536' : '#D2D4EF',
+            boxShadow: isLight ? 'none' : '10px 15px 20px 6px rgba(20, 0, 141, 0.1)',
+          }}
           id={'mouse-over-popover-goto'}
           title={'Go to MIPs Portal'}
         >
