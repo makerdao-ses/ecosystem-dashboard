b07b40dcb6e98ff0f49556906531bac07e941e56
Maykel Lopez <maykel.meneses89@gmail.com>
2022-08-11 13:16:30 +0200

    [SES-103] Added specific placeholder for breakdown tables

--- src/stories/containers/transparency-report/placeholders/transparency-empty-table.tsx
+++ src/stories/containers/transparency-report/placeholders/transparency-empty-table.tsx
@@ -1,59 +1,84 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-export const TransparencyEmptyTable = () => {
+interface Props {
+  breakdown?: boolean;
+}
+
+export const TransparencyEmptyTable = ({ breakdown = false }: Props) => {
   return (
     <Wrapper>
-      <Container>
-        <Row>
-          <CellBlock
-            style={{
-              marginRight: '111px',
-            }}
-          />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock />
-        </Row>
-        <Line />
-        {new Array(3).fill('').map((_, i) => (
-          <Row style={{ marginBottom: i === 2 ? '17px' : '30px' }}>
-            <Row style={{
-              gap: '0',
-              maxWidth: '238px'
-            }}>
-              <CellBlock
+      {!breakdown
+        ? (
+        <Container>
+          <Row>
+            <CellBlock
+              style={{
+                marginRight: '111px',
+              }}
+            />
+            <CellBlock />
+            <CellBlock />
+            <CellBlock />
+            <CellBlock />
+            <CellBlock />
+          </Row>
+          <Line />
+          {new Array(3).fill('').map((_, i) => (
+            <Row style={{ marginBottom: i === 2 ? '17px' : '30px' }}>
+              <Row
                 style={{
-                  borderRadius: '50%',
-                  maxWidth: '42px',
-                  height: '42px',
-                  marginRight: '5px',
-                }}
-              />
-              <div
-                style={{
-                  marginRight: '20px',
-                  maxWidth: '126px',
-                  width: '100%',
+                  gap: '0',
+                  maxWidth: '238px',
                 }}
               >
                 <CellBlock
                   style={{
-                    height: '22px',
-                    marginBottom: '6px',
-                    marginRight: '0',
+                    borderRadius: '50%',
+                    maxWidth: '42px',
+                    height: '42px',
+                    marginRight: '5px',
                   }}
                 />
-                <CellBlock
+                <div
                   style={{
-                    height: '16px',
-                    marginRight: '0',
+                    marginRight: '20px',
+                    maxWidth: '126px',
+                    width: '100%',
                   }}
-                />
-              </div>
+                >
+                  <CellBlock
+                    style={{
+                      height: '22px',
+                      marginBottom: '6px',
+                      marginRight: '0',
+                    }}
+                  />
+                  <CellBlock
+                    style={{
+                      height: '16px',
+                      marginRight: '0',
+                    }}
+                  />
+                </div>
+              </Row>
+              <CellBlock />
+              <CellBlock />
+              <CellBlock />
+              <CellBlock />
+              <CellBlock
+                style={{
+                  maxWidth: '262px',
+                }}
+              />
             </Row>
+          ))}
+          <Row>
+            <CellBlock
+              style={{
+                marginRight: '111px',
+              }}
+            />
             <CellBlock />
             <CellBlock />
             <CellBlock />
@@ -64,24 +89,176 @@ export const TransparencyEmptyTable = () => {
               }}
             />
           </Row>
-        ))}
-        <Row>
-          <CellBlock
+        </Container>
+          )
+        : (
+        <Container>
+          <Row>
+            <CellBlock
+              style={{
+                maxWidth: '137px',
+                marginRight: '151px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '105px',
+                marginRight: '55px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '106px',
+                marginRight: '54px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '105px',
+                marginRight: '21px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '109px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '109px',
+                marginRight: '188px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '126px',
+              }}
+            />
+          </Row>
+          <Line />
+          <Row style={{ marginBottom: '26px' }}>
+            <CellBlock
+              style={{
+                maxWidth: '230px',
+                height: '20px',
+              }}
+            />
+          </Row>
+          <Row
             style={{
-              marginRight: '111px',
+              justifyContent: 'space-between',
+              marginBottom: '34px',
             }}
-          />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock />
-          <CellBlock
+          >
+            <CellBlock
+              style={{
+                maxWidth: '137px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '190px',
+              }}
+            />
+          </Row>
+
+          <Row
             style={{
-              maxWidth: '262px',
+              marginBottom: '4px',
             }}
-          />
-        </Row>
-      </Container>
+          >
+            <CellBlock
+              style={{
+                maxWidth: '230px',
+                height: '20px',
+              }}
+            />
+          </Row>
+
+          <Row
+            style={{
+              marginBottom: '10px',
+            }}
+          >
+            <CellBlock
+              style={{
+                width: '79px',
+                height: '20px',
+              }}
+            />
+          </Row>
+
+          <Row style={{ marginBottom: '18px' }}>
+            <CellBlock
+              style={{
+                maxWidth: '158px',
+                marginRight: '116px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '199px',
+                marginRight: '37px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '180px',
+              }}
+            />
+          </Row>
+
+          <Row>
+            <CellBlock
+              style={{
+                maxWidth: '114px',
+                marginRight: '160px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                marginRight: '24px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '199px',
+                marginRight: '37px',
+              }}
+            />
+            <CellBlock
+              style={{
+                maxWidth: '180px',
+              }}
+            />
+          </Row>
+        </Container>
+          )}
       <Title>No Data Provided</Title>
     </Wrapper>
   );
