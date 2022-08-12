cbb1889a49b6e5dbbfc558271b6dabd973bafbbc
Maykel Lopez <maykel.meneses89@gmail.com>
2022-08-12 18:23:52 +0200

    [SES-82] Added SSR for transparency report

--- pages/core-unit/[code]/finances/reports/index.tsx
+++ pages/core-unit/[code]/finances/reports/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { CoreUnitDto } from '../../../../../src/core/models/dto/core-unit.dto';
 import { TransparencyReport } from '../../../../../src/stories/containers/transparency-report/transparency-report';
+import { useTransparencyReportViewModel } from '../../../../../src/stories/containers/transparency-report/transparency-report.mvvm';
 
-const Transparency = () => {
-  return <TransparencyReport/>;
+export const getServerSideProps = async() => {
+  const { data }: { data: any } = await useTransparencyReportViewModel('SES');
+
+  return {
+    props: {
+      cu: data && data.coreUnit[0],
+    },
+  };
+};
+
+const Transparency = ({ cu }: { cu: CoreUnitDto }) => {
+  return <TransparencyReport cu={cu} />;
 };
 
 export default Transparency;
