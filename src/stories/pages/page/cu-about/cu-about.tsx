c22f138bcab2b776a9ad5ea0fa204d9c6bb49f55
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-08-15 11:55:53 -0300

    fix-problem-name-story-book

--- src/stories/pages/page/cu-about/cu-about.tsx
+++ src/stories/pages/page/cu-about/cu-about.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import CuAboutContainer from '../../../containers/cu-about/cu-about-container';
 import { CuAbout } from '../../../containers/cu-about/cu-about.api';
-const CuAbout = () => {
+const CuAboutPageTesting = () => {
   return (
     <CuAboutContainer cuAbout={{} as CuAbout} code='SES-001' contributors={[]}/>);
 };
 
-export default CuAbout;
+export default CuAboutPageTesting;
