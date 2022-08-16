e446ef79e24e2fe6e3492462f0d1aeef94a4a354
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-08-16 17:57:38 -0300

    Add-icons-to-manifest-file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,17 +29,16 @@ function MyApp(props: MyAppProps) {
         <Head>
           <title>MakerDAO - Dashboard</title>
           <link rel="icon" href="/favicon.png" />
-          <meta name='mobile-web-app-capable' content='yes'/>
-          <meta name='apple-mobile-web-app-capable' content='yes'/>
-
-          <link rel='apple-touch-icon' href='favicon_152x152.png'/>
-          <meta name='msapplication-square150x150logo' content='favicon_152x152.png'/>
-          </Head>
-          <FeatureFlagsProvider enabledFeatures={featureFlags[CURRENT_ENVIRONMENT]}>
-            <Component {...pageProps} />
-          </FeatureFlagsProvider>
-        </Provider>
-      </ThemeProvider>
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <link rel='apple-touch-icon' href='favicon_152x152.png' />
+          <meta name='msapplication-square150x150logo' content='favicon_152x152.png' />
+        </Head>
+        <FeatureFlagsProvider enabledFeatures={featureFlags[CURRENT_ENVIRONMENT]}>
+          <Component {...pageProps} />
+        </FeatureFlagsProvider>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
