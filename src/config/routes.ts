3eee365336399367b9100ef024554342f205ce21
YasielCabrera <yasiel9506@gmail.com>
2022-08-17 16:31:51 -0400

    remove unused routes constants

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,4 +1,1 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://expenses.makerdao.network';
-export const HOME_ROUTE = '/';
-export const ABOUT_ROUTE = '/about/:code';
-export const TRANSPARENCY_REPORT_ROUTE = '/transparency';
