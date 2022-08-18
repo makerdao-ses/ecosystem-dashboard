dfd6f856918a01ad7b7e50a2c2730c46c6a0eece
Maykel Lopez <maykel.meneses89@gmail.com>
2022-08-18 13:01:40 +0200

    [SES-91] Fix on cells padding

--- src/stories/components/number-cell/number-cell.tsx
+++ src/stories/components/number-cell/number-cell.tsx
@@ -13,18 +13,21 @@ interface NumberCellProps {
 
 export const NumberCell = ({ responsivePadding = '10px 16px', ...props }: NumberCellProps) => {
   const isLight = useThemeContext().themeMode === 'light';
-  return <Container fontFamily={props.fontFamily} isLight={isLight} style={{
+  return <Container responsivePadding={responsivePadding} fontFamily={props.fontFamily} isLight={isLight} style={{
     fontWeight: props.bold ? 700 : 400,
-    padding: responsivePadding || '16px',
     ...props.style
   }} negative={props.value < 0}>{formatNumber(props.value)}</Container>;
 };
 
-const Container = styled.div<{ negative?: boolean, fontFamily?: string, isLight: boolean }>(({ negative = false, fontFamily = 'SF Pro Display, sans-serif', isLight }) => ({
+const Container = styled.div<{ negative?: boolean, fontFamily?: string, isLight: boolean, responsivePadding: string }>(({ negative = false, fontFamily = 'SF Pro Display, sans-serif', isLight, responsivePadding }) => ({
   fontFamily,
   fontSize: '14px',
   lineHeight: '17px',
+  padding: responsivePadding,
   color: isLight && negative ? '#F75524' : isLight && !negative ? '#231536' : !isLight && negative ? '#F75524' : '#D2D4EF',
+  '@media (min-width: 834px)': {
+    padding: '10px 16px',
+  },
   '@media (min-width: 1194px)': {
     fontSize: '16px',
     lineHeight: '19px',
