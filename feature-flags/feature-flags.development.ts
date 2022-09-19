7e96dc41302940e7f3c9b58a1d1f8ac09fd46a21
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-09-19 10:43:54 -0300

    Add new feature flags container

--- feature-flags/feature-flags.development.ts
+++ feature-flags/feature-flags.development.ts
@@ -5,4 +5,5 @@ export const flagsDevelopment: FeatureFlagsInterface = {
   FEATURE_SITEMAP: process.env.ENABLE_SITEMAP === 'true',
   FEATURE_TRANSPARENCY_NEW_TABLE: true,
   FEATURE_CU_INDEX_NEW_TABLE: true,
+  FEATURE_CU_ABOUT_NEW_CONTAINER: true,
 };
