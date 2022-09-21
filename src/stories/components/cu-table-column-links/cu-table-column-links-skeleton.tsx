3096c605f88c472762cd8e8d890a4322e2611b2e
Maykel Lopez <maykel.meneses89@gmail.com>
2022-09-21 14:06:00 +0200

    [SES-147] Added responsive skeleton for loading

--- src/stories/components/cu-table-column-links/cu-table-column-links-skeleton.tsx
+++ src/stories/components/cu-table-column-links/cu-table-column-links-skeleton.tsx
@@ -7,15 +7,38 @@ export const ColumnLinksSkeleton = () => {
   const isLight = useThemeContext().themeMode === 'light';
   return (
     <Container>
-      <Skeleton
-        variant="rectangular"
-        width={260}
-        height={28}
-        style={{
-          borderRadius: '8px',
-          background: isLight ? '#ECF1F3' : '#1E2C37',
-        }}
-      />
+      <CompactWrapper>
+        <Skeleton
+          variant="rectangular"
+          width={140}
+          height={20}
+          style={{
+            borderRadius: '8px',
+            background: isLight ? '#ECF1F3' : '#1E2C37',
+            marginBottom: '8px',
+          }}
+        />
+        <Skeleton
+          variant="rectangular"
+          width={140}
+          height={20}
+          style={{
+            borderRadius: '8px',
+            background: isLight ? '#ECF1F3' : '#1E2C37',
+          }}
+        />
+      </CompactWrapper>
+      <NormalWrapper>
+        <Skeleton
+          variant="rectangular"
+          width={260}
+          height={28}
+          style={{
+            borderRadius: '8px',
+            background: isLight ? '#ECF1F3' : '#1E2C37',
+          }}
+        />
+      </NormalWrapper>
     </Container>
   );
 };
@@ -23,4 +46,19 @@ export const ColumnLinksSkeleton = () => {
 const Container = styled.div({
   display: 'flex',
   margin: '40px 22px 30px',
+});
+
+const CompactWrapper = styled.div({
+  flexDirection: 'column',
+  display: 'none',
+  '@media (min-width: 1194px) and (max-width: 1409px)': {
+    display: 'flex',
+  },
+});
+
+const NormalWrapper = styled.div({
+  display: 'flex',
+  '@media (min-width: 1194px) and (max-width: 1409px)': {
+    display: 'none !important',
+  },
 });
