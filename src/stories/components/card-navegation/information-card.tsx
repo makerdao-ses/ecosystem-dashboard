b7ce4ce19a78f519ee7dd1440ce98187a6125e0d
Alejandro Cabriales <alejandro.cabriales@dspot.com.pl>
2022-09-30 18:23:27 -0300

    Fix-error-card

--- src/stories/components/card-navegation/information-card.tsx
+++ src/stories/components/card-navegation/information-card.tsx
@@ -64,10 +64,6 @@ const Container = styled(Box, { shouldForwardProp: (prop) => prop !== 'isLight' 
   isLight: boolean;
 }>(({ isLight }) => ({
   background: isLight ? '#FFFFFF' : '#10191F',
-  boxShadow: isLight
-    ? '0px 20px 40px rgba(219, 227, 237, 0.4), 0px 1px 3px rgba(190, 190, 190, 0.25)'
-    : '10px 15px 20px 6px rgba(20, 0, 141, 0.1)',
-  borderRadius: '6px',
   [lightTheme.breakpoints.between('table_834', 'desktop_1194')]: {
     width: '100%',
   },
