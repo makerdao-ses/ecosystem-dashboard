e4a248c696d14c6b830d4fab6f27c537a6fa802c
Maykel Lopez <maykel.meneses89@gmail.com>
2022-09-30 15:07:28 +0200

    [SES-143] Enabling feature flags for staging

--- feature-flags/feature-flags.staging.ts
+++ feature-flags/feature-flags.staging.ts
@@ -3,8 +3,8 @@ import { FeatureFlagsInterface } from './feature-flags.interface';
 export const flagsStaging: FeatureFlagsInterface = {
   FEATURE_CARD_NAVIGATION: true,
   FEATURE_SITEMAP: process.env.ENABLE_SITEMAP === 'true',
-  FEATURE_TRANSPARENCY_NEW_TABLE: false,
-  FEATURE_CU_INDEX_NEW_TABLE: false,
-  FEATURE_CU_ABOUT_NEW_CONTAINER: false,
-  FEATURE_TRANSPARENCY_COMMENTS: false,
+  FEATURE_TRANSPARENCY_NEW_TABLE: true,
+  FEATURE_CU_INDEX_NEW_TABLE: true,
+  FEATURE_CU_ABOUT_NEW_CONTAINER: true,
+  FEATURE_TRANSPARENCY_COMMENTS: true,
 };
