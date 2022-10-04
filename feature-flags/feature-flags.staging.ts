56c05e306139edd112a5c8328fe50d82273b7d48
Maykel Lopez <maykel.meneses89@gmail.com>
2022-10-04 12:59:31 +0200

    [NO-TICKET] Enabling production flags

--- feature-flags/feature-flags.staging.ts
+++ feature-flags/feature-flags.staging.ts
@@ -2,7 +2,7 @@ import { FeatureFlagsInterface } from './feature-flags.interface';
 
 export const flagsStaging: FeatureFlagsInterface = {
   FEATURE_CARD_NAVIGATION: true,
-  FEATURE_SITEMAP: process.env.ENABLE_SITEMAP === 'true',
+  FEATURE_SITEMAP: true,
   FEATURE_TRANSPARENCY_NEW_TABLE: true,
   FEATURE_CU_INDEX_NEW_TABLE: true,
   FEATURE_CU_ABOUT_NEW_CONTAINER: true,
