7232612f07753be781766f1c79dd638ac7d76284
Maykel Lopez <maykel.meneses89@gmail.com>
2022-10-22 12:00:14 +0200

    [SES-189] Added validations according to client requirements

--- src/stories/containers/login/login.mvvm.ts
+++ src/stories/containers/login/login.mvvm.ts
@@ -2,14 +2,38 @@
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
+const charactersNotAllowedMessage =
+  'Characters not allowed. Only letters, numbers, and the following characters are allowed: !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
 const validationSchema = yup.object({
   username: yup
     .string()
     .required('Username is required and this is a really long string to check how it works with multiple lines'),
   password: yup
     .string()
-    .min(8, 'Password should be of minimum 8 characters length')
-    .matches(/thisthis/, 'Password can only contain Latin letters.')
+    .min(10, 'Your password must have at least 10 characters.')
+    .matches(/[^a-z]/g, 'Your password must contain at least one uppercase character, number, or special character.')
+    .matches(/[^A-Z]/g, 'Your password must contain at least one lowercase character, number, or special character.')
+    .matches(
+      /[^0-9]/g,
+      'Your password must contain at least one uppercase character, lowercase character, or special character.'
+    )
+    .matches(
+      /[^!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g,
+      'Your password must contain at least one lowercase character, uppercase character, or number.'
+    )
+    .matches(
+      /^((([a-z]+[A-Z]+)+)|(([A-Z]+[a-z]+)+)|(([a-z]+[0-9]+)+)||(([0-9]+[a-z]+)+)|(([A-Z]+[0-9]+)+)|(([0-9]+[A-Z]+)+))[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]*$/g,
+      charactersNotAllowedMessage
+    )
+    .matches(
+      /^((([!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+[A-Z]+)+)|(([A-Z]+[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+)+)|(([a-z]+[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+)+)||(([!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+[a-z]+)+)|(([A-Z]+[0-9]+)+)|(([0-9]+[A-Z]+)+))[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]*$/g,
+      charactersNotAllowedMessage
+    )
+    .matches(
+      /^((([!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+[0-9]+)+)|(([0-9]+[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+)+))[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]*$/g,
+      charactersNotAllowedMessage
+    )
     .required('Password is required'),
 });
 
