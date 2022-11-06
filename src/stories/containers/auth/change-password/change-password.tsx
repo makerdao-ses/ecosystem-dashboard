f5c897f5b93c78ad30483311995d6d49dfe6b8b8
Maykel Lopez <maykel.meneses89@gmail.com>
2022-11-06 21:13:57 +0100

    [SES-189] Fixes to change password view

--- src/stories/containers/auth/change-password/change-password.tsx
+++ src/stories/containers/auth/change-password/change-password.tsx
@@ -3,27 +3,36 @@ import React from 'react';
 import { CustomButton } from '../../../components/custom-button/custom-button';
 import AvatarPlaceholder from '../../../components/svg/avatar-placeholder';
 import TextInput from '../../../components/text-input/text-input';
-import { ButtonWrapper, Container, InputsWrapper, Wrapper } from '../login/login';
+import { ButtonWrapper, Container, Wrapper } from '../login/login';
 
 export default () => {
   return (
     <Wrapper>
       <Container>
         <AvatarPlaceholder />
-        <Username>Wouter Kampman</Username>
+        <UserWrapper>
+          <UserLabel>Username:</UserLabel>
+          <Username>Wouter Kampman</Username>
+        </UserWrapper>
         <ChangePassword>Change Your Password</ChangePassword>
         <InputsWrapper>
           <Label>Enter Existing Password</Label>
           <TextInput type="password" placeholder="Password" name="OldPassword" style={{ marginBottom: 32 }} />
           <Label>Enter New Password</Label>
-          <TextInput type="password" placeholder="Password" name="NewPassword" style={{ marginBottom: 24 }} />
-          <TextInput type="password" placeholder="Password" name="ConfirmPassword" style={{ marginBottom: 32 }} />
+          <TextInput type="password" placeholder="New Password" name="NewPassword" style={{ marginBottom: 24 }} />
+          <TextInput
+            type="password"
+            placeholder="Confirm Password"
+            name="ConfirmPassword"
+            style={{ marginBottom: 32 }}
+          />
         </InputsWrapper>
         <ButtonWrapper>
           <CustomButton
             label="Set New Password"
             style={{
               width: 174,
+              height: 34,
               borderRadius: 22,
             }}
           />
@@ -33,21 +42,35 @@ export default () => {
   );
 };
 
+const UserWrapper = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  marginTop: 16,
+  marginBottom: 48,
+});
+
+const UserLabel = styled.p({
+  color: '#708390',
+  fontSize: 24,
+  lineHeight: '24px',
+  fontWeight: 600,
+  margin: '0 8px 0 0',
+});
+
 const Username = styled.h1({
   fontSize: 24,
   lineHeight: '29px',
   color: '#231536',
-  marginTop: 16,
-  marginBottom: 48,
   textAlign: 'center',
+  margin: 0,
 });
 
 const ChangePassword = styled.h2({
   fontSize: 20,
   lineHeight: '24px',
   color: '#231536',
-  marginBottom: 32,
-  textAlign: 'center',
+  margin: '0 0 32px 0',
+  alignSelf: 'flex-start',
   letterSpacing: 0.4,
 });
 
@@ -57,4 +80,12 @@ const Label = styled.div({
   lineHeight: '22px',
   color: '#231536',
   marginBottom: 16,
+});
+
+const InputsWrapper = styled.div({
+  width: '100%',
+  marginBottom: 0,
+  '@media (min-width: 834px)': {
+    marginBottom: 32,
+  },
 });
