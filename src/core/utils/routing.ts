ecff6dcebac30a4045928eacab7c4712c4f8dcfe
Yasiel Cabrera <yasiel9506@gmail.com>
2022-11-27 12:21:28 -0500

    removed unnecessary console.log

--- src/core/utils/routing.ts
+++ src/core/utils/routing.ts
@@ -1,9 +1,6 @@
 import Router from 'next/router';
 
 export const goBack = (defaultRoute?: string): void => {
-  console.log('goBack');
-  console.log('window?.history?.length', window?.history?.length);
-  console.log('window?.history?.state', window?.history?.state?.idx);
   if (window?.history?.state?.idx > 0) {
     Router.back();
   } else {
