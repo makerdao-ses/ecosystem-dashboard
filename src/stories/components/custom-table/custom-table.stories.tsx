f4345fa7b9244dc79e28689138634815de7dcbbf
Yasiel Cabrera <yasiel9506@gmail.com>
2023-01-10 10:06:55 -0500

    Integrate with chromatic

--- src/stories/components/custom-table/custom-table.stories.tsx
+++ src/stories/components/custom-table/custom-table.stories.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { CustomTable } from './custom-table';
+import { store } from '../../../../src/core/store/store';
+import { Provider } from 'react-redux';
 
 export default {
   title: 'Components/General/CustomTable',
   component: CustomTable,
 } as ComponentMeta<typeof CustomTable>;
 
-const Template: ComponentStory<typeof CustomTable> = (args) => <CustomTable {...args} />;
+const Template: ComponentStory<typeof CustomTable> = (args) => (
+  <Provider store={store}>
+    <CustomTable {...args} />
+  </Provider>
+);
 
 export const Default = Template.bind({});
 Default.args = {
