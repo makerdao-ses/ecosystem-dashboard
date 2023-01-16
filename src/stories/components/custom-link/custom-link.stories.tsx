3887b00f6a1da06054f2773fab82ff4291118c8e
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-01-16 18:00:43 -0300

    Add storybook with figma data

--- src/stories/components/custom-link/custom-link.stories.tsx
+++ src/stories/components/custom-link/custom-link.stories.tsx
@@ -1,3 +1,5 @@
+import { withFigmaComparator } from '@ses/core/utils/storybook/decorators';
+import { figmaComparatorCommonPaddingOptions } from '@ses/core/utils/storybook/utils';
 import React from 'react';
 import { CustomLink } from './custom-link';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
@@ -13,3 +15,10 @@ export const Default = Template.bind({});
 Default.args = {
   children: 'Custom link',
 };
+
+Default.decorators = [
+  withFigmaComparator(
+    'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=2108%3A17916&t=dMZEfODgYNIpeDoi-0',
+    figmaComparatorCommonPaddingOptions
+  ),
+];
