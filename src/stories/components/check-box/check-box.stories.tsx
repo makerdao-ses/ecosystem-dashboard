0e7f73a564fd2579335e609ff5b2a9718a17f5a1
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-01-23 13:47:29 -0300

    Add check banner to storybook

--- src/stories/components/check-box/check-box.stories.tsx
+++ src/stories/components/check-box/check-box.stories.tsx
@@ -1,7 +1,5 @@
 import CheckBox from '@ses/components/check-box/check-box';
-import { withFigmaComparator } from '@ses/core/utils/storybook/decorators';
 import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
-import { figmaComparatorCommonPaddingOptions } from '@ses/core/utils/storybook/utils';
 import type { ComponentMeta } from '@storybook/react';
 export default {
   title: 'Components/General/CheckBox',
@@ -24,15 +22,29 @@ export const [[Checked, CheckedDarkMode], [Unchecked, UncheckedDarkMode]] = crea
   variantsArgs
 );
 
-Checked.decorators = [
-  withFigmaComparator(
-    'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17690&t=0QkX2DAbV9YY8AId-4',
-    figmaComparatorCommonPaddingOptions
-  ),
-];
-Unchecked.decorators = [
-  withFigmaComparator(
-    'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17688&t=0QkX2DAbV9YY8AId-4',
-    figmaComparatorCommonPaddingOptions
-  ),
-];
+Checked.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17690&t=0QkX2DAbV9YY8AId-4',
+  },
+};
+
+CheckedDarkMode.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17690&t=0QkX2DAbV9YY8AId-4',
+  },
+};
+Unchecked.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17690&t=0QkX2DAbV9YY8AId-4',
+  },
+};
+
+UncheckedDarkMode.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=3384%3A17690&t=0QkX2DAbV9YY8AId-4',
+  },
+};
--- src/stories/components/cookies-policy-banner/cookies-policy-banner.stories.tsx
+++ src/stories/components/cookies-policy-banner/cookies-policy-banner.stories.tsx
@@ -1,7 +1,5 @@
 import CookiesPolicyBanner from '@ses/components/cookies-policy-banner/cookies-policy-banner';
-import { withFigmaComparator } from '@ses/core/utils/storybook/decorators';
 import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
-// import { figmaComparatorCommonPaddingOptions } from '@ses/core/utils/storybook/utils';
 import type { ComponentMeta } from '@storybook/react';
 
 export default {
@@ -38,37 +36,28 @@ const variantsArgs = [
 
 export const [[Banner, BannerDarkMode]] = createThemeModeVariants(CookiesPolicyBanner, variantsArgs);
 
-Banner.decorators = [
-  withFigmaComparator({
-    0: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A98025&t=MIuynuM3mpq7bvEM-4',
-    834: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97378&t=MIuynuM3mpq7bvEM-4',
-    1194: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97359&t=MIuynuM3mpq7bvEM-4',
-    1280: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97340&t=MIuynuM3mpq7bvEM-4',
-    1440: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A92795&t=MIuynuM3mpq7bvEM-4',
-    1920: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A94290&t=MIuynuM3mpq7bvEM-4',
-  }),
-];
+Banner.parameters = {
+  figma: {
+    component: {
+      0: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119658&t=feJZwRzZjZ2QfeYW-4',
+      834: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119619&t=feJZwRzZjZ2QfeYW-4',
+      1194: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119600&t=feJZwRzZjZ2QfeYW-4',
+      1280: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119581&t=feJZwRzZjZ2QfeYW-4',
+      1440: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119562&t=feJZwRzZjZ2QfeYW-4',
+      1920: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119543&t=feJZwRzZjZ2QfeYW-4',
+    },
+  },
+};
 
-BannerDarkMode.decorators = [
-  withFigmaComparator({
-    0: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A98025&t=MIuynuM3mpq7bvEM-4',
-    834: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97378&t=MIuynuM3mpq7bvEM-4',
-    1194: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97359&t=MIuynuM3mpq7bvEM-4',
-    1280: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A97340&t=MIuynuM3mpq7bvEM-4',
-    1440: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A92795&t=MIuynuM3mpq7bvEM-4',
-    1920: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9838%3A94290&t=MIuynuM3mpq7bvEM-4',
-  }),
-];
-
-// Banner.decorators = [
-//   withFigmaComparator(
-//     'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9522%3A96615&t=d9UdVXKB5mStSiIv-0',
-//     figmaComparatorCommonPaddingOptions
-//   ),
-// ];
-// BannerDarkMode.decorators = [
-//   withFigmaComparator(
-//     'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=9522%3A96615&t=d9UdVXKB5mStSiIv-0',
-//     figmaComparatorCommonPaddingOptions
-//   ),
-// ];
+BannerDarkMode.parameters = {
+  figma: {
+    component: {
+      0: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119658&t=feJZwRzZjZ2QfeYW-4',
+      834: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119619&t=feJZwRzZjZ2QfeYW-4',
+      1194: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119600&t=feJZwRzZjZ2QfeYW-4',
+      1280: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119581&t=feJZwRzZjZ2QfeYW-4',
+      1440: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119562&t=feJZwRzZjZ2QfeYW-4',
+      1920: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=12894%3A119543&t=feJZwRzZjZ2QfeYW-4',
+    },
+  },
+};
