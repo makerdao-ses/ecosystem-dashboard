dfe894f1b982173c8d05708afc4b477d41283c9a
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-01-23 11:06:29 -0300

    Add expenditures to storybook

--- src/stories/components/cu-table-column-expenditures/cu-table-column-expenditures.stories.tsx
+++ src/stories/components/cu-table-column-expenditures/cu-table-column-expenditures.stories.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
 import { CuTableColumnExpenditures } from './cu-table-column-expenditures';
-import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import type { ComponentMeta } from '@storybook/react';
 
 export default {
   title: 'Components/CUTable/ColumnExpenditures',
   component: CuTableColumnExpenditures,
 } as ComponentMeta<typeof CuTableColumnExpenditures>;
 
-const Template: ComponentStory<typeof CuTableColumnExpenditures> = (args) => <CuTableColumnExpenditures {...args} />;
+const variantsArgs = [
+  {
+    value: 16705,
+    percent: 96,
+    items: [{ value: 70 }, { value: 60 }, { value: 90 }],
+    budgetCaps: [90, 80, 100],
+  },
+];
 
-export const Default = Template.bind({});
-Default.args = {
-  value: 16500,
-  percent: 120,
-  items: [{ value: 70 }, { value: 85 }, { value: 120 }],
-  budgetCaps: [90, 80, 100],
+export const [[Expenditures, ExpendituresDark]] = createThemeModeVariants(CuTableColumnExpenditures, variantsArgs);
+
+Expenditures.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=8853%3A101706&t=vtK1PenCnIfiA5DY-4',
+  },
+};
+ExpendituresDark.parameters = {
+  figma: {
+    component:
+      'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=8853%3A101706&t=vtK1PenCnIfiA5DY-4',
+  },
 };
