e53f5b936b0511c5a3cc741903d69c60e4b53c6b
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-01-26 18:29:26 -0300

    Fix-erros-new-way-of-story

--- src/stories/components/custom-table-header/custom-table-header.stories.tsx
+++ src/stories/components/custom-table-header/custom-table-header.stories.tsx
@@ -1,6 +1,4 @@
-import { withFigmaComparator } from '@ses/core/utils/storybook/decorators';
 import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
-import { figmaComparatorCommonPaddingOptions } from '@ses/core/utils/storybook/utils';
 import { SortEnum } from '../../../core/enums/sort.enum';
 import { CustomTableHeader } from './custom-table-header';
 import type { ComponentMeta } from '@storybook/react';
@@ -20,17 +18,3 @@ const variantsArgs = [
 ];
 
 export const [[Header, HeaderDarkMode]] = createThemeModeVariants(CustomTableHeader, variantsArgs);
-
-Header.decorators = [
-  withFigmaComparator(
-    'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=6646%3A61302&t=iDXzm6LhfULmvnWw-4',
-    figmaComparatorCommonPaddingOptions
-  ),
-];
-
-HeaderDarkMode.decorators = [
-  withFigmaComparator(
-    'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?node-id=6646%3A61302&t=iDXzm6LhfULmvnWw-4',
-    figmaComparatorCommonPaddingOptions
-  ),
-];
