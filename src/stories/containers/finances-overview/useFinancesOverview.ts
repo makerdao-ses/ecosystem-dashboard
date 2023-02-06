e24faad69601033762789b4302bccdc263e9dc08
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-02-06 17:09:07 -0300

    Add border in each bar

--- src/stories/containers/finances-overview/useFinancesOverview.ts
+++ src/stories/containers/finances-overview/useFinancesOverview.ts
@@ -4,6 +4,92 @@ import { useState } from 'react';
 import type { SeriesDataApi } from '@ses/core/models/dto/chart.dto';
 
 const useFinancesOverview = () => {
+  const mockData = [
+    {
+      period: '2023-01',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 12465122.0,
+      discontinued: 13512500.0,
+    },
+    {
+      period: '2023-02',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 12465122.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-03',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 12465122.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-04',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 1230000.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-05',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 34234343.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-06',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 23234343.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-07',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 4451000.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-08',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 23212343.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-09',
+      budget: '/makerdao/core-units',
+      prediction: 13512500.0,
+      actuals: 3234343.0,
+      discontinued: 15132650.0,
+    },
+    {
+      period: '2023-10',
+      budget: '/makerdao/core-units',
+      prediction: 0.0,
+      actuals: 1934343.0,
+      discontinued: 3932650.0,
+    },
+    {
+      period: '2023-11',
+      budget: '/makerdao/core-units',
+      prediction: 0,
+      actuals: 3334343.0,
+      discontinued: 433432,
+    },
+    {
+      period: '2023-12',
+      budget: '/makerdao/core-units',
+      prediction: 41345123.0,
+      actuals: 34545456,
+      discontinued: 0,
+    },
+  ];
   const [selectedYear, setSelectedYear] = useState<number>(() => DateTime.local().minus({ year: 1 }).year);
   const { isLight } = useThemeContext();
   const years = [2021, 2022, 2023];
@@ -24,12 +110,54 @@ const useFinancesOverview = () => {
     };
   };
 
+  const valuesForChart = processDataPerMonth(mockData, DateTime.fromISO('2023-01'));
+  const noneBorder = [0, 0, 0, 0];
+  const lowerBorder = [0, 0, 6, 6];
+  const superiorBorder = [6, 6, 0, 0];
+  const fullBorder = [16, 16, 46, 46];
+
+  const newActual = valuesForChart.actuals.map((item, index: number) => ({
+    name: 'Active Budget',
+    value: item,
+    itemStyle: {
+      borderRadius:
+        valuesForChart.discontinued[index] === 0 && valuesForChart.prediction[index] === 0 ? fullBorder : lowerBorder,
+    },
+  }));
+
+  const newDiscontinued = valuesForChart.discontinued.map((item, index: number) => ({
+    name: 'Discontinued',
+    value: item,
+    itemStyle: {
+      borderRadius:
+        valuesForChart.actuals[index] === 0 && valuesForChart.prediction[index] === 0
+          ? fullBorder
+          : valuesForChart.actuals[index] !== 0 && valuesForChart.prediction[index] !== 0
+          ? noneBorder
+          : valuesForChart.actuals[index] !== 0 && valuesForChart.prediction[index] === 0
+          ? superiorBorder
+          : lowerBorder,
+    },
+  }));
+
+  const newPrediction = valuesForChart.prediction.map((item, index: number) => ({
+    name: 'Expense forecasts',
+    value: item,
+    itemStyle: {
+      borderRadius:
+        valuesForChart.actuals[index] === 0 && valuesForChart.prediction[index] === 0 ? fullBorder : superiorBorder,
+    },
+  }));
+
   return {
     isLight,
     selectedYear,
     years,
     handleChangeSelectYear,
     processDataPerMonth,
+    newDiscontinued,
+    newPrediction,
+    newActual,
   };
 };
 
