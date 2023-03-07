bd8289d6f8a6d757a6cd0d968b81889c13dcf062
Yasiel Cabrera <yasiel9506@gmail.com>
2023-03-07 11:39:19 -0500

    Fix broken stories

--- src/stories/containers/global-activity/global-activity.stories.tsx
+++ src/stories/containers/global-activity/global-activity.stories.tsx
@@ -1,4 +1,4 @@
-import { ActivityBuilder } from '@ses/core/business-logic/builders/activity.builder';
+import { ActivityBuilder } from '@ses/core/business-logic/builders/activityBuilder';
 import { CoreUnitsBuilder } from '@ses/core/business-logic/builders/coreUnitsBuilder';
 import { withoutSBPadding } from '@ses/core/utils/storybook/decorators';
 import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
