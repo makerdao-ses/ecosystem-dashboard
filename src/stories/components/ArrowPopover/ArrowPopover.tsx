20b2122bd6327b49e474b94967b9066be46fefc8
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-03-13 14:28:02 -0300

    Delete unnecesary type

--- src/stories/components/ArrowPopover/ArrowPopover.tsx
+++ src/stories/components/ArrowPopover/ArrowPopover.tsx
@@ -2,9 +2,8 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 import type { PropsWithChildren } from 'react';
-type Props = PropsWithChildren;
 
-const ArrowPopover: React.FC<Props> = ({ children }) => <Container>{children}</Container>;
+const ArrowPopover: React.FC<PropsWithChildren> = ({ children }) => <Container>{children}</Container>;
 export default ArrowPopover;
 
 const Container = styled.div({
