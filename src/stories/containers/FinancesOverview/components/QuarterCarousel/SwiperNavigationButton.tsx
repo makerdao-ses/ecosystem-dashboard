8ad5e6f75bf85a2b02951e045f1674ca0aa7f47f
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-05-03 14:53:44 -0300

    Add navigation component

---
+++ src/stories/containers/FinancesOverview/components/QuarterCarousel/SwiperNavigationButton.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import ArrowSwiperNext from '@ses/components/svg/ArrowSwiperNext';
+import ArrowSwiperPrevious from '@ses/components/svg/ArrowSwiperPrevious';
+import React from 'react';
+
+interface Props {
+  onNext?: () => void;
+  onPrevious?: () => void;
+}
+
+const SwiperNavigationButton: React.FC<Props> = ({ onNext, onPrevious }) => (
+  <Container>
+    <ContainerButtonLeft>
+      <ArrowSwiperPrevious onClick={onPrevious} />
+    </ContainerButtonLeft>
+    <ContainerButtonRight>
+      <ArrowSwiperNext onClick={onNext} />
+    </ContainerButtonRight>
+  </Container>
+);
+
+export default SwiperNavigationButton;
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+});
+
+const ContainerButtonRight = styled.div({
+  marginRight: -30,
+});
+
+const ContainerButtonLeft = styled.div({
+  marginLeft: -30,
+});
