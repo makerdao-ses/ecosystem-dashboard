a7f325cc425c6883f5a805a20ae12bd1d14fd5dd
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-06-26 09:37:42 -0300

    Fix add hook

--- src/stories/containers/Actors/useActors.ts
+++ src/stories/containers/Actors/useActors.ts
@@ -1,8 +1,14 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
 import lightTheme from '@ses/styles/theme/light';
 import { useState } from 'react';
+import { filteredActors } from './utils/utils';
+import type { MultiSelectItem } from '@ses/components/CustomMultiSelect/CustomMultiSelect';
+import type { EcosystemActor } from '@ses/core/models/dto/teamsDTO';
 
-export const useActors = () => {
+export const useActors = (actors: EcosystemActor[]) => {
+  // Remove this after implementation
+  console.log('actors', actors);
+  const [activeElements, setActiveElements] = useState<string[]>([]);
   const phone = useMediaQuery(lightTheme.breakpoints.up('desktop_1194'));
   const isLessPhone = useMediaQuery(lightTheme.breakpoints.down(376));
   const [readMore, setReadMore] = useState<boolean>(false);
@@ -11,10 +17,27 @@ export const useActors = () => {
     setReadMore(!readMore);
   };
 
+  const selectElements = [] as MultiSelectItem[];
+
+  const clearFilters = () => {
+    setActiveElements([]);
+  };
+
+  const filtersActive = activeElements.length <= 0 ? actors : filteredActors(actors, activeElements);
+
+  const handleSelectChange = (value: string[]) => {
+    setActiveElements(value);
+  };
+
   return {
     handleRead,
     readMore,
     showTextDesk,
     isLessPhone,
+    selectElements,
+    clearFilters,
+    handleSelectChange,
+    activeElements,
+    filtersActive,
   };
 };
