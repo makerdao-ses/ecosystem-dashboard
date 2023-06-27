a64a01a97ffc0ce65c386ec112c4e113af4ba0ed
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-06-27 12:01:02 -0300

    Add necesary data for the item

---
+++ src/core/enums/actorScopeEnum.ts
@@ -0,0 +1,7 @@
+export enum ActorScopeEnum {
+  SupportScope = 'Support Scope',
+  StabilityScope = 'Stability Scope',
+  AccessibilityScope = 'Accessibility Scope',
+  ProtocolScope = 'Protocol Scope',
+  GovernanceScope = 'Governance Scope',
+}
