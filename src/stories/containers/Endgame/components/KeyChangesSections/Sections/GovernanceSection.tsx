ef5befc4ecce8e8a6b3d4c7d294a9e98d121af49
Yasiel Cabrera <yasiel9506@gmail.com>
2023-08-17 09:20:15 -0400

    hotfix: Fixed copies

--- src/stories/containers/Endgame/components/KeyChangesSections/Sections/GovernanceSection.tsx
+++ src/stories/containers/Endgame/components/KeyChangesSections/Sections/GovernanceSection.tsx
@@ -19,13 +19,13 @@ const GovernanceSection: React.FC = () => (
       ]}
     >
       Due to voter apathy within MakerDAO governance and DAOs more broadly, the DAO will transition from a model of
-      recognized delegates to two groups aligned voter committees, and aligned delegates. MKR holders who are interested
-      in Maker governance can now participate in voter committees of their liking to discuss policy with the relevant
-      delegates.
+      recognized delegates to two groups consisting of aligned voter committees, and aligned delegates. MKR holders who
+      are interested in Maker governance can now participate in voter committees of their liking to discuss policy with
+      the relevant delegates.
     </KeyChangeCard>
     <KeyChangeCard
       from="Maker Improvement Proposals (MIPs)"
-      to={['The Maker Atlas', '5 Scope Artifacts']}
+      to={['The Maker Atlas', 'Five Scope Artifacts']}
       title="Refocusing on core business activities"
       readMore={[
         {
@@ -40,7 +40,7 @@ const GovernanceSection: React.FC = () => (
     >
       Before Endgame, Maker Improvement Proposals allowed any change to Maker Governance without much restriction or
       direction. The Endgame plan introduces the Maker Atlas which defines much better what the core business of the
-      Maker Protocol is all about. It also introduces 5 Scope Artifacts which defines how Maker should operate within
+      Maker Protocol is all about. It also introduces five Scope Artifacts which defines how Maker should operate within
       different operational areas.
     </KeyChangeCard>
   </>
