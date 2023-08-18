fc9ce6c4e728096cefb35674e5a30fbc1c75fbd9
Yasiel Cabrera <yasiel9506@gmail.com>
2023-08-18 11:03:41 -0400

    feat: add total budget UI

---
+++ src/stories/containers/Endgame/components/BudgetStructureSection/BudgetStructureSection.stories.tsx
@@ -0,0 +1,83 @@
+import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
+import BudgetStructureSection from './BudgetStructureSection';
+import type { ComponentMeta } from '@storybook/react';
+import type { FigmaParams } from 'storybook-addon-figma-comparator/dist/ts/types';
+
+export default {
+  title: 'Components/Endgame/Budget Structure Section',
+  component: BudgetStructureSection,
+  parameters: {
+    chromatic: {
+      viewports: [375, 834, 1194, 1280, 1440],
+      pauseAnimationAtEnd: true,
+    },
+  },
+} as ComponentMeta<typeof BudgetStructureSection>;
+
+const variantsArgs = [{}];
+
+export const [[LightMode, DarkMode]] = createThemeModeVariants(BudgetStructureSection, variantsArgs);
+
+LightMode.parameters = {
+  figma: {
+    component: {
+      0: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=21089:238282',
+        options: {
+          componentStyle: {
+            width: 343,
+          },
+          style: {
+            top: 0,
+            left: -40,
+          },
+        },
+      },
+      834: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=21235:239248',
+        options: {
+          componentStyle: {
+            width: 834,
+          },
+          style: {
+            top: 0,
+            left: -40,
+          },
+        },
+      },
+      1194: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=20947:243382',
+        options: {
+          style: {
+            top: 0,
+            left: -40,
+          },
+        },
+      },
+      1280: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=20872:236912',
+        options: {
+          componentStyle: {
+            width: 1184,
+          },
+          style: {
+            top: 0,
+            left: -40,
+          },
+        },
+      },
+      1440: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=20804:270289',
+        options: {
+          componentStyle: {
+            width: 1312,
+          },
+          style: {
+            top: 0,
+            left: -40,
+          },
+        },
+      },
+    },
+  } as FigmaParams,
+};
