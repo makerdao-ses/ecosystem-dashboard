78ec63ed049c2c2467c4f9939200cf9c7dc52b8c
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-08-18 09:04:31 -0300

    Add correct place for stories and add card resume for mobile

---
+++ src/stories/containers/Finances/components/OverviewCardMobile/OverviewCardMobile.stories.tsx
@@ -0,0 +1,45 @@
+import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
+import OverviewCardMobile from './OverviewCardMobile';
+import type { ComponentMeta } from '@storybook/react';
+import type { FigmaParams } from 'storybook-addon-figma-comparator/dist/ts/types';
+
+export default {
+  title: 'Components/NewFinances/OverviewCardMobile',
+  component: OverviewCardMobile,
+
+  parameters: {
+    chromatic: {
+      viewports: [357],
+      pauseAnimationAtEnd: true,
+    },
+  },
+} as ComponentMeta<typeof OverviewCardMobile>;
+
+const args = [
+  {
+    actuals: 9120,
+    budgetCap: 9120,
+    prediction: 4436,
+  },
+];
+export const [[LightMode, DarkMode]] = createThemeModeVariants(OverviewCardMobile, args);
+
+LightMode.parameters = {
+  figma: {
+    component: {
+      357: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22857:196036&mode=dev',
+        options: {
+          componentStyle: {
+            width: 343,
+          },
+          style: {
+            top: -20,
+            left: -40,
+          },
+        },
+      },
+    },
+  } as FigmaParams,
+};
