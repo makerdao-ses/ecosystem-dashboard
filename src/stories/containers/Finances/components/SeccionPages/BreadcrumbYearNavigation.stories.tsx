1ab945f9d810cd0be885df38299b263bd3137550
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-08-22 21:56:50 -0300

    Add responsive for ScrumbNavigation

--- src/stories/containers/Finances/components/SeccionPages/BreadcrumbYearNavigation.stories.tsx
+++ src/stories/containers/Finances/components/SeccionPages/BreadcrumbYearNavigation.stories.tsx
@@ -10,7 +10,7 @@ export default {
 
   parameters: {
     chromatic: {
-      viewports: [1440],
+      viewports: [375, 1440],
       pauseAnimationAtEnd: true,
     },
   },
@@ -37,12 +37,77 @@ export const [[LightMode, DarkMode]] = createThemeModeVariants(BreadcrumbYearNav
 LightMode.parameters = {
   figma: {
     component: {
+      375: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22820:193423&mode=dev',
+        options: {
+          componentStyle: {
+            width: 375,
+          },
+          style: {
+            top: 16,
+            left: 16,
+          },
+        },
+      },
+      834: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22977:248499&mode=dev',
+        options: {
+          componentStyle: {
+            width: 834,
+          },
+          style: {
+            top: -1,
+            left: -1,
+          },
+        },
+      },
+      1194: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22944:242472&mode=dev',
+        options: {
+          componentStyle: {
+            width: 1194,
+          },
+          style: {
+            top: -1,
+            left: -1,
+          },
+        },
+      },
+      1280: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22935:211743&mode=dev',
+        options: {
+          componentStyle: {
+            width: 1280,
+          },
+          style: {
+            top: -1,
+            left: -1,
+          },
+        },
+      },
       1440: {
         component:
           'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22069:258713&mode=dev',
         options: {
           componentStyle: {
             width: 1440,
+          },
+          style: {
+            top: -1,
+            left: -1,
+          },
+        },
+      },
+      1920: {
+        component:
+          'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=22935:205711&mode=dev',
+        options: {
+          componentStyle: {
+            width: 1920,
           },
           style: {
             top: -1,
