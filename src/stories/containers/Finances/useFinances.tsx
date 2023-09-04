7bfd4cda45dc0838cd5dadd4f38544a1e8b99219
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2023-09-04 08:47:39 -0300

    Add new order method for the headers

--- src/stories/containers/Finances/useFinances.tsx
+++ src/stories/containers/Finances/useFinances.tsx
@@ -1,26 +1,46 @@
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useThemeContext } from '@ses/core/context/ThemeContext';
+import { SortEnum } from '@ses/core/enums/sortEnum';
 import lightTheme from '@ses/styles/theme/light';
+import orderBy from 'lodash/orderBy';
 import sortBy from 'lodash/sortBy';
+import { DateTime } from 'luxon';
 import { useRouter } from 'next/router';
 import { useMemo, useState } from 'react';
 import EndgameAtlasBudgets from './components/EndgameAtlasBudgets';
 import EndgameScopeBudgets from './components/EndgameScopeBudgets';
 import MakerDAOLegacyBudgets from './components/MakerDAOLegacyBudgets';
-import type { FilterDoughnut, NavigationCard, PeriodicSelectionFilter } from './utils/types';
+import { getExpenseMonthWithData, mockDataApiTeam } from './utils/utils';
+import type {
+  DelegateExpenseTableHeader,
+  FilterDoughnut,
+  NavigationCard,
+  PeriodicSelectionFilter,
+} from './utils/types';
 import type { SelectChangeEvent } from '@mui/material/Select';
 import type { MultiSelectItem } from '@ses/components/CustomMultiSelect/CustomMultiSelect';
+
 import type { DoughnutSeries } from '@ses/core/models/interfaces/doughnutSeries';
+import type { MomentDataItem } from '@ses/core/models/interfaces/team';
 
 export const useFinances = () => {
   const { isLight } = useThemeContext();
   const isMobile = useMediaQuery(lightTheme.breakpoints.down('table_834'));
+  const getExpenseReportItems: MomentDataItem[] = useMemo(() => mockDataApiTeam, []);
   const routes = ['Finances'];
   const years = ['2022', '2023'];
   const metricsFilter = useMemo(
     () => ['Budget', 'Actual', 'Forecast', 'Net Expenses On-chain', 'Net Expenses Off-chain'],
     []
   );
+  const [sortColumn, setSortColumn] = useState<number>(-1);
+  const [headersSort, setHeadersSort] = useState<SortEnum[]>([
+    SortEnum.Asc,
+    SortEnum.Neutral,
+    SortEnum.Neutral,
+    SortEnum.Neutral,
+    SortEnum.Neutral,
+  ]);
 
   const [activeElements, setActiveElements] = useState<string[]>([]);
   const handleSelectChangeMetrics = (value: string[]) => {
@@ -65,6 +85,149 @@ export const useFinances = () => {
   const handleClosePeriod = () => {
     setIsOpenPeriod(false);
   };
+
+  const headersExpenseReport: DelegateExpenseTableHeader[] = [
+    {
+      header: 'Contributors',
+      styles: {
+        boxSizing: 'border-box',
+        minWidth: 228,
+        paddingLeft: 16,
+      },
+      sortReverse: true,
+      sort: headersSort[0],
+    },
+    {
+      header: 'Reporting Month',
+      styles: {
+        width: 170,
+        marginLeft: 112,
+
+        [lightTheme.breakpoints.up('desktop_1280')]: {
+          marginLeft: 122,
+        },
+        [lightTheme.breakpoints.up('desktop_1440')]: {
+          marginLeft: 126,
+        },
+      },
+      sortReverse: true,
+      sort: headersSort[1],
+    },
+    {
+      header: 'Total Actuals',
+      sort: headersSort[2],
+      styles: {
+        width: 170,
+        marginLeft: -18,
+
+        [lightTheme.breakpoints.up('desktop_1280')]: {
+          marginLeft: -4,
+        },
+        [lightTheme.breakpoints.up('desktop_1440')]: {
+          marginLeft: 12,
+          justifyContent: 'center',
+        },
+      },
+      sortReverse: true,
+    },
+    {
+      header: 'Status',
+      sort: headersSort[3],
+      styles: {
+        width: 173,
+        marginLeft: -6,
+
+        [lightTheme.breakpoints.up('desktop_1280')]: {
+          // marginLeft: 18,
+          marginLeft: 2,
+        },
+        [lightTheme.breakpoints.up('desktop_1440')]: {
+          marginLeft: 12,
+          justifyContent: 'center',
+        },
+      },
+      sortReverse: true,
+    },
+    {
+      header: 'Last Modified',
+      sort: headersSort[4],
+      styles: {
+        width: 173,
+        marginLeft: 10,
+
+        [lightTheme.breakpoints.up('desktop_1280')]: {
+          marginLeft: 22,
+        },
+        [lightTheme.breakpoints.up('desktop_1440')]: {
+          marginLeft: 92,
+          justifyContent: 'center',
+        },
+      },
+      sortReverse: true,
+    },
+  ];
+  const sortData = useMemo(() => {
+    const sortDataFunction = (items: MomentDataItem[]) => {
+      if (headersSort[sortColumn] === SortEnum.Disabled) return items;
+
+      const multiplier = headersSort[sortColumn] === SortEnum.Asc ? 1 : -1;
+
+      const name = (a: MomentDataItem, b: MomentDataItem) => a.name.localeCompare(b.name) * multiplier;
+      const month = (a: MomentDataItem, b: MomentDataItem) =>
+        (a.reportMonth.toMillis() - b.reportMonth.toMillis()) * multiplier;
+      const total = (a: MomentDataItem, b: MomentDataItem) => (a.totalActuals - b.totalActuals) * multiplier;
+
+      const status = (a: MomentDataItem, b: MomentDataItem): number => {
+        const statusA = a.budgetStatements[a.budgetStatements.length - 1]?.status || '';
+        const statusB = b.budgetStatements[b.budgetStatements.length - 1]?.status || '';
+        return statusA.localeCompare(statusB) * multiplier;
+      };
+
+      const lastModified = (a: MomentDataItem, b: MomentDataItem) => {
+        const lastModifiedA = getExpenseMonthWithData(a)?.toMillis() ?? DateTime.fromJSDate(new Date()).toMillis();
+        const lastModifiedB = getExpenseMonthWithData(b)?.toMillis() ?? DateTime.fromJSDate(new Date()).toMillis();
+        return (lastModifiedA - lastModifiedB) * multiplier;
+      };
+
+      const sortAlg = [name, month, total, status, lastModified, () => 0];
+      return [...items].sort(sortAlg[sortColumn]);
+    };
+    return sortDataFunction;
+  }, [headersSort, sortColumn]);
+
+  const groupByStatusDefaultSorting: MomentDataItem[] = useMemo(() => {
+    const resultMoment = orderBy(getExpenseReportItems, 'name');
+
+    return resultMoment;
+  }, [getExpenseReportItems]);
+  const reportExpenseItems: MomentDataItem[] = useMemo(() => {
+    const sortedData = sortData(groupByStatusDefaultSorting);
+    return sortedData?.map((x: MomentDataItem) => ({
+      ...x,
+      value: x,
+      key: x.code,
+    }));
+  }, [groupByStatusDefaultSorting, sortData]);
+
+  const onSortClick = (index: number) => {
+    const sortNeutralState = headersExpenseReport.map((header) =>
+      header.sort ? SortEnum.Neutral : SortEnum.Disabled
+    ) as SortEnum[];
+
+    if (headersSort[index] === SortEnum.Neutral) {
+      if (headersExpenseReport[index].sortReverse) {
+        sortNeutralState[index] = SortEnum.Desc;
+      } else {
+        sortNeutralState[index] = SortEnum.Asc;
+      }
+    } else {
+      sortNeutralState[index] = headersSort[index] === SortEnum.Asc ? SortEnum.Desc : SortEnum.Asc;
+    }
+
+    setHeadersSort(sortNeutralState);
+    setSortColumn(index);
+  };
+
   const selectMetrics = useMemo(
     () =>
       sortBy(metricsFilter, (item) => item).map((filter) => ({
@@ -134,6 +297,10 @@ export const useFinances = () => {
       color: '#2DC1B1',
     },
   ];
+  const handleLinkToPage = (href: string) => {
+    // Add the correct link when APi is ready
+    console.log('some links', href);
+  };
   return {
     years,
     year,
@@ -162,5 +329,9 @@ export const useFinances = () => {
     handleSelectChangeMetrics,
     selectMetrics,
     handleResetMetrics,
+    headersExpenseReport,
+    onSortClick,
+    reportExpenseItems,
+    handleLinkToPage,
   };
 };
