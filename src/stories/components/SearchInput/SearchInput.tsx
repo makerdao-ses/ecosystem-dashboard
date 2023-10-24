46a2751c52557e5af8ece68788047f6e8680d6c2
Yasiel Cabrera <yasiel9506@gmail.com>
2023-10-24 17:21:34 -0400

    feat: project page subheader

--- src/stories/components/SearchInput/SearchInput.tsx
+++ src/stories/components/SearchInput/SearchInput.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 import { useMediaQuery } from '@mui/material';
+import lightTheme from '@ses/styles/theme/light';
 import React, { useState } from 'react';
 import { useThemeContext } from '../../../core/context/ThemeContext';
 import { Close } from '../svg/close';
 import Magnifier from '../svg/magnifier';
+import type { WithLegacyBreakpoints } from '@ses/core/utils/typesHelpers';
 import type { CSSProperties } from 'react';
-interface SearchInputProps {
+interface SearchInputProps extends Partial<WithLegacyBreakpoints> {
   value?: string;
   defaultValue?: string;
   placeholder: string;
@@ -16,42 +18,47 @@ interface SearchInputProps {
   small?: boolean;
 }
 
-export const SearchInput = (props: SearchInputProps) => {
+export const SearchInput: React.FC<SearchInputProps> = ({
+  value,
+  defaultValue,
+  placeholder,
+  onChange,
+  style,
+  handleClearSearch,
+  inputRef,
+  small,
+  legacyBreakpoints = true,
+}) => {
   const { isLight } = useThemeContext();
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.onChange && props.onChange(event.target.value);
+    onChange && onChange(event.target.value);
   };
 
-  const isDesktop = useMediaQuery('(min-width: 834px)');
+  const isDesktop = useMediaQuery(lightTheme.breakpoints.up(legacyBreakpoints ? 'table_834' : 'tablet_768'));
 
   const [focus, setFocus] = useState(false);
 
   return (
-    <Container style={props.style}>
+    <Container style={style}>
       <InputWrapper>
         <Input
-          ref={props.inputRef}
+          legacyBreakpoints={legacyBreakpoints}
+          ref={inputRef}
           isLight={isLight}
           id="search-input"
           onChange={handleChange}
-          placeholder={props.placeholder}
+          placeholder={placeholder}
           onFocus={() => setFocus(true)}
           onBlur={() => setFocus(false)}
-          focus={focus || !!props.value}
-          value={props.value}
-          defaultValue={props.defaultValue}
-          small={props.small}
+          focus={focus || !!value}
+          value={value}
+          defaultValue={defaultValue}
+          small={small}
           autoComplete="off"
         />
         <IconWrapper>
-          {focus || !!props.value || !!props.defaultValue ? (
-            <Close
-              onClick={props.handleClearSearch}
-              width={10}
-              height={10}
-              fill="#25273D"
-              fillDark="rgb(237, 239, 255)"
-            />
+          {focus || !!value || !!defaultValue ? (
+            <Close onClick={handleClearSearch} width={10} height={10} fill="#25273D" fillDark="rgb(237, 239, 255)" />
           ) : (
             <Magnifier
               fill={isLight ? '#25273D' : '#ADAFD4'}
@@ -76,40 +83,43 @@ const InputWrapper = styled.div({
   width: 'min(100%, 330px)',
 });
 
-const Input = styled.input<{ focus: boolean; isLight: boolean; small?: boolean }>(({ focus, isLight, small }) => ({
-  fontFamily: 'Inter, sans-serif',
-  fontStyle: 'normal',
-  fontWeight: 500,
-  fontSize: '14px',
-  flex: 1,
-  color: isLight ? '#25273D' : '#FFFFFF',
-  outline: 'none',
-  width: '100%',
-  height: '34px',
-  border:
-    isLight && focus
-      ? '1px solid #231536'
-      : isLight && !focus
-      ? '1px solid #D4D9E1'
-      : !isLight && focus
-      ? '1px solid #787A9B'
-      : '1px solid #343442',
-  borderRadius: '22px',
-  padding: '15px 45px 15px 16px',
-  boxSizing: 'border-box',
-  transition: 'all .3s ease',
-  backgroundColor: isLight ? '#FFFFFF' : '#10191F',
-  '&::placeholder': {
-    color: isLight ? '#B0BCC0' : '#D2D4EF',
-  },
-  '@media (min-width: 834px)': !small
-    ? {
-        width: '320px',
-        height: '48px',
-        fontSize: '14px',
-      }
-    : undefined,
-}));
+const Input = styled.input<{ focus: boolean; isLight: boolean; small?: boolean } & WithLegacyBreakpoints>(
+  ({ focus, isLight, small, legacyBreakpoints }) => ({
+    fontFamily: 'Inter, sans-serif',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '14px',
+    flex: 1,
+    color: isLight ? '#25273D' : '#FFFFFF',
+    outline: 'none',
+    width: '100%',
+    height: '34px',
+    border:
+      isLight && focus
+        ? '1px solid #231536'
+        : isLight && !focus
+        ? '1px solid #D4D9E1'
+        : !isLight && focus
+        ? '1px solid #787A9B'
+        : '1px solid #343442',
+    borderRadius: '22px',
+    padding: '15px 45px 15px 16px',
+    boxSizing: 'border-box',
+    transition: 'all .3s ease',
+    backgroundColor: isLight ? '#FFFFFF' : '#10191F',
+    '&::placeholder': {
+      color: isLight ? '#B0BCC0' : '#D2D4EF',
+    },
+
+    [lightTheme.breakpoints.up(legacyBreakpoints ? 'table_834' : 'tablet_768')]: !small
+      ? {
+          width: '320px',
+          height: '48px',
+          fontSize: '14px',
+        }
+      : undefined,
+  })
+);
 
 const IconWrapper = styled.div({
   position: 'absolute',
---
+++ src/stories/containers/ActorProjects/components/PageSubheader/PageSubheader.stories.tsx
@@ -0,0 +1,55 @@
+import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
+import PageSubheader from './PageSubheader';
+import type { ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'Components/Actor/ProjectsPageSubheader',
+  component: PageSubheader,
+  parameters: {
+    chromatic: {
+      viewports: [375, 768, 1024, 1280],
+    },
+  },
+} as ComponentMeta<typeof PageSubheader>;
+
+const variantsArgs = [{}];
+export const [[LightMode, DarkMode]] = createThemeModeVariants(PageSubheader, variantsArgs);
+
+LightMode.parameters = {
+  figma: {
+    component: {
+      375: {
+        component: 'https://www.figma.com/file/nubRdNZLDrQJLDgh8caMrQ/Untitled?type=design&node-id=1233:18779',
+        options: {
+          componentStyle: {
+            width: 343,
+          },
+        },
+      },
+      768: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=26315:267176',
+        options: {
+          componentStyle: {
+            width: 704,
+          },
+        },
+      },
+      1024: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=26314:262204',
+        options: {
+          componentStyle: {
+            width: 960,
+          },
+        },
+      },
+      1280: {
+        component: 'https://www.figma.com/file/pyaYEjcwF2b5uf9y0vIfIy/SES-Dashboard?type=design&node-id=26314:256298',
+        options: {
+          componentStyle: {
+            width: 1184,
+          },
+        },
+      },
+    },
+  },
+};
