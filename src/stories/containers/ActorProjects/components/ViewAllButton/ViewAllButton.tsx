e57575973181ff53bf54ab59c5d14737e4f65d5c
Yasiel Cabrera <yasiel9506@gmail.com>
2023-11-03 12:06:59 -0400

    feat: expand/collapse deliverables

--- src/stories/containers/ActorProjects/components/ViewAllButton/ViewAllButton.tsx
+++ src/stories/containers/ActorProjects/components/ViewAllButton/ViewAllButton.tsx
@@ -3,13 +3,24 @@ import { useThemeContext } from '@ses/core/context/ThemeContext';
 import React from 'react';
 import type { WithIsLight } from '@ses/core/utils/typesHelpers';
 
-const ViewAllButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+interface ViewAllButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  viewAll: boolean;
+}
+
+const ViewAllButton: React.FC<ViewAllButtonProps> = (props) => {
   const { isLight } = useThemeContext();
 
   return (
     <Button isLight={isLight} {...props}>
       <span>{props.children}</span>
-      <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        style={{ transform: `rotate(${props.viewAll ? '180' : 0}deg)` }}
+        width="17"
+        height="16"
+        viewBox="0 0 17 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
           d="M8.19339 10.8631C8.35404 11.0456 8.64598 11.0456 8.80664 10.8631L13.4036 5.63952C13.6255 5.38735 13.4398 5 13.097 5H3.90306C3.56023 5 3.37451 5.38735 3.59643 5.63952L8.19339 10.8631Z"
           fill="#25273D"
