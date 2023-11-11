9773422def70c6057b6f88f5748febac76c755f4
Yasiel Cabrera <yasiel9506@gmail.com>
2023-11-11 09:05:02 -0500

    feat: add empty state

--- src/stories/containers/ActorProjects/components/ProjectList/ProjectList.tsx
+++ src/stories/containers/ActorProjects/components/ProjectList/ProjectList.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { TablePlaceholder } from '@ses/components/CustomTable/TablePlaceholder';
 import React from 'react';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import type { Project } from '@ses/core/models/interfaces/projects';
@@ -12,6 +13,10 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects }) => (
     {projects.map((project) => (
       <ProjectCard key={project.id} project={project} />
     ))}
+
+    {projects.length === 0 && (
+      <TablePlaceholder description="There are no Projects available with this combination of filters." />
+    )}
   </List>
 );
 
