dd791f35cc451b95ece61aeb163c29f6c901bd53
Manuel Alejandro <96558830+alejandrocabriales@users.noreply.github.com>
2023-12-13 18:09:57 -0300

    Isses/Add new year (#1146)
    * Add new year
    
    * Remove the console.log

--- src/stories/containers/Finances/utils/utils.ts
+++ src/stories/containers/Finances/utils/utils.ts
@@ -782,7 +782,7 @@ export const processingData = (series: { value: number }[]) => {
 
 export const getYearsRange = () => {
   const year = DateTime.utc().year;
-  const yearsRange = Array(1 + year - 2022)
+  const yearsRange = Array(1 + year - 2021)
     .fill('')
     .map((_, i) => (year - i).toString());
   return yearsRange;
