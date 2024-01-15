1938cc74b7f10df656c0613589ce735a02d27c28
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-01-15 10:06:55 -0300

    Add default icon

--- src/stories/containers/Finances/useFinances.tsx
+++ src/stories/containers/Finances/useFinances.tsx
@@ -71,7 +71,7 @@ export const useFinances = (budgets: Budget[], initialYear: string) => {
         : [newBudgetMetric()];
 
     return {
-      image: item.image || '',
+      image: item.image || '/assets/img/default-icon-cards-budget.svg',
       title: removePrefix(item.name, prefixToRemove),
       description: item.description || 'Finances of the core governance constructs described in the Maker Atlas.',
       href: `${siteRoutes.newFinancesOverview}/${item.codePath.replace('atlas/', '')}?year=${year}`,
