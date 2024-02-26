5802e58669601783499bce79f594a38b7503fb49
Manuel Alejandro <96558830+alejandrocabriales@users.noreply.github.com>
2024-02-26 16:28:58 -0300

    Add css breakpoint to avoid render container before loading (#1322)

--- src/stories/containers/Finances/components/SectionPages/BreakdownTable/BreakdownTableSkeleton/BreakdownTableBodySkeleton/HeaderRowSkeleton.tsx
+++ src/stories/containers/Finances/components/SectionPages/BreakdownTable/BreakdownTableSkeleton/BreakdownTableBodySkeleton/HeaderRowSkeleton.tsx
@@ -1,14 +1,8 @@
-import { Skeleton, styled, useMediaQuery } from '@mui/material';
+import { Skeleton, styled } from '@mui/material';
 import React from 'react';
-import type { Theme } from '@mui/material';
+import HeaderRowSkeletonTitle from './HeaderRowSkeletonTitle';
 
 export const HeaderRowSkeleton = () => {
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('tablet_768'));
-  const isTablet = useMediaQuery((theme: Theme) => theme.breakpoints.between('tablet_768', 'desktop_1024'));
-  const isDesk1024 = useMediaQuery((theme: Theme) => theme.breakpoints.between('desktop_1024', 'desktop_1280'));
-  const isDesk1280 = useMediaQuery((theme: Theme) => theme.breakpoints.between('desktop_1280', 'desktop_1440'));
-  const isDesk1440 = useMediaQuery((theme: Theme) => theme.breakpoints.up('desktop_1440'));
-
   const TwoItemsSkeleton = () => (
     <TwoItemsSkeletonContainer>
       <ItemStyledSkeleton
@@ -33,248 +27,144 @@ export const HeaderRowSkeleton = () => {
   return (
     <Container>
       <TitleBox>
-        {isMobile && (
-          <>
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={38}
-              height={9.62}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={51}
-              height={9.62}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={39}
-              height={9.62}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-          </>
-        )}
-        {isTablet && (
-          <>
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={119}
-              height={12.15}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={90}
-              height={12.15}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-          </>
-        )}
-        {isDesk1024 && (
-          <>
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={90}
-              height={12.15}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={119}
-              height={12.15}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-          </>
-        )}
-        {isDesk1280 && (
-          <>
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={190}
-              height={12.15}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-          </>
-        )}
-        {isDesk1440 && (
-          <>
-            <ItemStyledSkeleton
-              variant="rectangular"
-              width={160}
-              height={12.51}
-              sx={{
-                borderRadius: 13.5,
-              }}
-            />
-          </>
-        )}
+        <HeaderRowSkeletonTitle />
       </TitleBox>
+      <Mobile>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={56}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={56}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={56}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+      </Mobile>
 
-      <>
-        {isMobile && (
-          <ItemsBox>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={56}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={56}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={56}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-          </ItemsBox>
-        )}
-        {isTablet && (
-          <ItemsBox>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={61}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={61}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={61}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={61}
-                height={9.62}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-            <ContainerItem>
-              <ItemStyledSkeleton
-                variant="rectangular"
-                width={61}
-                height={10.5}
-                sx={{
-                  borderRadius: 13.5,
-                }}
-              />
-            </ContainerItem>
-          </ItemsBox>
-        )}
-        {isDesk1024 && (
-          <ItemsBox>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-          </ItemsBox>
-        )}
-        {isDesk1280 && (
-          <ItemsBox>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-          </ItemsBox>
-        )}
-        {isDesk1440 && (
-          <ItemsBox>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-            <ContainerItem>
-              <TwoItemsSkeleton />
-            </ContainerItem>
-          </ItemsBox>
-        )}
-      </>
+      <Tablet>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={61}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={61}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={61}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={61}
+            height={9.62}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+        <ContainerItem>
+          <ItemStyledSkeleton
+            variant="rectangular"
+            width={61}
+            height={10.5}
+            sx={{
+              borderRadius: 13.5,
+            }}
+          />
+        </ContainerItem>
+      </Tablet>
+      <Desk1024>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+      </Desk1024>
+      <Desk1280>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+      </Desk1280>
+      <Desk1440>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+        <ContainerItem>
+          <TwoItemsSkeleton />
+        </ContainerItem>
+      </Desk1440>
     </Container>
   );
 };
@@ -286,7 +176,9 @@ const Container = styled('div')(({ theme }) => ({
   width: '100%',
   padding: '16px 8px',
   borderRadius: 6,
+  minHeight: 48,
   overflow: 'hidden',
+  alignItems: 'center',
   background: theme.palette.mode === 'light' ? 'rgba(236, 239, 249, 0.50)' : 'rgb(64, 83, 97, 0.30)',
   [theme.breakpoints.up('tablet_768')]: {
     padding: '16px 0px 16px 8px',
@@ -328,40 +220,20 @@ const ContainerItem = styled('div')(({ theme }) => ({
   flexDirection: 'row',
   justifyContent: 'center',
   alignItems: 'center',
-  minWidth: 76,
-  flex: 1,
-  [theme.breakpoints.up('tablet_768')]: {
-    minWidth: 78,
-  },
-  [theme.breakpoints.up('desktop_1024')]: {
-    minWidth: 130,
-  },
-  [theme.breakpoints.up('desktop_1280')]: {
-    minWidth: 130,
-  },
-  [theme.breakpoints.up('desktop_1440')]: {
-    minWidth: 130,
-  },
-}));
 
-const ItemsBox = styled('div')(({ theme }) => ({
-  flex: 3 / 4,
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
+  width: 76,
+
   [theme.breakpoints.up('tablet_768')]: {
-    minWidth: 'calc(100%-145px)',
-    flex: 5 / 6,
+    minWidth: 100,
   },
   [theme.breakpoints.up('desktop_1024')]: {
-    minWidth: 'calc(100%-142px)',
+    minWidth: 130,
   },
   [theme.breakpoints.up('desktop_1280')]: {
-    minWidth: 'calc(100%-228px)',
+    minWidth: 180,
   },
   [theme.breakpoints.up('desktop_1440')]: {
-    minWidth: 'calc(100%-228px)',
+    minWidth: 180,
   },
 }));
 
@@ -376,4 +248,62 @@ const TwoItemsSkeletonContainer = styled('div')(({ theme }) => ({
 
 const ItemStyledSkeleton = styled(Skeleton)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'light' ? '#D1DEE6' : '#31424E',
+}));
+
+const Mobile = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: 3 / 4,
+
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  [theme.breakpoints.up('tablet_768')]: {
+    display: 'none',
+  },
+}));
+const Tablet = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.between('tablet_768', 'desktop_1024')]: {
+    display: 'flex',
+    flex: 5 / 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    minWidth: 'calc(100%-145px)',
+  },
+}));
+
+const Desk1024 = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.between('desktop_1024', 'desktop_1280')]: {
+    display: 'flex',
+    flex: 5 / 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    minWidth: 'calc(100%-142px)',
+  },
+}));
+
+const Desk1280 = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.between('desktop_1280', 'desktop_1440')]: {
+    display: 'flex',
+    flex: 5 / 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    minWidth: 'calc(100%-180px)',
+  },
+}));
+const Desk1440 = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.up('desktop_1440')]: {
+    display: 'flex',
+    flex: 5 / 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    minWidth: 'calc(100%-228px)',
+  },
 }));
--- src/stories/containers/Finances/components/SectionPages/BreakdownTable/BreakdownTableSkeleton/HeaderTitleSkeleton.tsx
+++ src/stories/containers/Finances/components/SectionPages/BreakdownTable/BreakdownTableSkeleton/HeaderTitleSkeleton.tsx
@@ -1,34 +1,46 @@
-import { Skeleton, styled, useMediaQuery } from '@mui/material';
+import { Skeleton, styled } from '@mui/material';
 import React from 'react';
-import type { Theme } from '@mui/material';
 
-const HeaderTitleSkeleton = () => {
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('tablet_768'));
-  const isTable = useMediaQuery((theme: Theme) => theme.breakpoints.between('tablet_768', 'desktop_1024'));
-  const isDesk1024 = useMediaQuery((theme: Theme) => theme.breakpoints.between('desktop_1024', 'desktop_1280'));
-  const isDesk1028 = useMediaQuery((theme: Theme) => theme.breakpoints.up('desktop_1280'));
-  return (
-    <Container>
-      {(isMobile || isTable) && (
-        <>
-          <ItemSkeleton variant="rectangular" />
-          <ItemSkeleton variant="rectangular" />
-        </>
-      )}
-      {isDesk1024 && (
-        <>
-          <ItemSkeleton variant="rectangular" width={84} />
-          <ItemSkeleton variant="rectangular" width={136} />
-        </>
-      )}
-      {isDesk1028 && <ItemSkeleton variant="rectangular" width={145} />}
-    </Container>
-  );
-};
+const HeaderTitleSkeleton = () => (
+  <Container>
+    <Mobile>
+      <ItemSkeleton variant="rectangular" />
+      <ItemSkeleton variant="rectangular" />
+    </Mobile>
+    <Table>
+      <ItemSkeleton variant="rectangular" />
+      <ItemSkeleton variant="rectangular" />
+    </Table>
 
+    <Desk1024>
+      <ItemSkeleton variant="rectangular" width={84} />
+      <ItemSkeleton variant="rectangular" width={136} />
+    </Desk1024>
+
+    <Desk1028>
+      <ItemSkeleton variant="rectangular" width={145} />
+    </Desk1028>
+  </Container>
+);
 export default HeaderTitleSkeleton;
 
-const Container = styled('div')(({ theme }) => ({
+const Container = styled('div')({
+  display: 'flex',
+});
+
+const ItemSkeleton = styled(Skeleton)(({ theme }) => ({
+  borderRadius: 15,
+  height: 10.5,
+  width: 50,
+  backgroundColor: theme.palette.mode === 'light' ? '#D1DEE6' : '#31424E',
+  [theme.breakpoints.up('tablet_768')]: {
+    borderRadius: 20,
+    height: 14,
+    width: 98,
+  },
+}));
+
+const Mobile = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
@@ -44,44 +56,68 @@ const Container = styled('div')(({ theme }) => ({
     borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
   },
   [theme.breakpoints.up('tablet_768')]: {
-    width: 136,
-    ':after': {
-      content: '""',
-      position: 'absolute',
-      height: 48,
-      right: 0,
-      borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
-    },
+    display: 'none',
   },
-  [theme.breakpoints.up('desktop_1024')]: {
-    ':after': {
-      content: '""',
-      position: 'absolute',
+}));
+const Table = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.between('tablet_768', 'desktop_1024')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 16,
+    position: 'relative',
+    [theme.breakpoints.up('tablet_768')]: {
+      width: 136,
       height: 48,
-      right: -16,
-      borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
-    },
-  },
-  [theme.breakpoints.up('desktop_1280')]: {
-    width: 228,
-    ':after': {
-      content: '""',
-      position: 'absolute',
-      height: 48,
-      right: 10,
-      borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
+      ':after': {
+        content: '""',
+        position: 'absolute',
+        height: 48,
+        right: 0,
+        borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
+      },
     },
   },
 }));
 
-const ItemSkeleton = styled(Skeleton)(({ theme }) => ({
-  borderRadius: 15,
-  height: 10.5,
-  width: 50,
-  backgroundColor: theme.palette.mode === 'light' ? '#D1DEE6' : '#31424E',
-  [theme.breakpoints.up('tablet_768')]: {
-    borderRadius: 20,
-    height: 14,
-    width: 98,
+const Desk1024 = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.between('desktop_1024', 'desktop_1280')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 16,
+    position: 'relative',
+    [theme.breakpoints.up('desktop_1024')]: {
+      ':after': {
+        content: '""',
+        position: 'absolute',
+        height: 48,
+        right: -16,
+        borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
+      },
+    },
+  },
+}));
+
+const Desk1028 = styled('div')(({ theme }) => ({
+  display: 'none',
+  [theme.breakpoints.up('desktop_1280')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    position: 'relative',
+    gap: 16,
+    [theme.breakpoints.up('desktop_1280')]: {
+      width: 228,
+      ':after': {
+        content: '""',
+        position: 'absolute',
+        height: 48,
+        right: 10,
+        borderRight: `1px solid ${theme.palette.mode === 'light' ? '#D1DEE6' : 'rgb(84, 105, 120, 0.30)'}`,
+      },
+    },
   },
 }));
