0f79cb82f3b15485f49bb43276dfaae40d203b89
Yasiel Cabrera <yasiel9506@gmail.com>
2024-04-11 11:00:04 -0600

    fix: sort navigation cards on mobile

--- src/stories/containers/Finances/useFinances.tsx
+++ src/stories/containers/Finances/useFinances.tsx
@@ -107,25 +107,27 @@ export const useFinances = (budgets: Budget[], allBudgets: Budget[], initialYear
   // All the logic required by the CardNavigation section
   const cardsNavigationInformation = useMemo(
     () =>
-      budgets.map((item, index) => {
-        const budgetMetric =
-          budgetsAnalytics !== undefined && budgetsAnalytics[item.codePath] !== undefined
-            ? budgetsAnalytics[item.codePath]
-            : [newBudgetMetric()];
+      budgets
+        .map((item, index) => {
+          const budgetMetric =
+            budgetsAnalytics !== undefined && budgetsAnalytics[item.codePath] !== undefined
+              ? budgetsAnalytics[item.codePath]
+              : [newBudgetMetric()];
 
-        return {
-          image: item.image || '/assets/img/default-icon-cards-budget.svg',
-          codePath: item.codePath,
-          title: formatBudgetName(item.name),
-          description: item.description || 'Finances of the core governance constructs described in the Maker Atlas.',
-          href: `${siteRoutes.finances(item.codePath.replace('atlas/', ''))}?year=${year}`,
-          valueDai: budgetMetric[0].paymentsOnChain.value,
-          totalDai: allMetrics.paymentsOnChain,
-          code: item.code,
-          color: isLight ? colorsLight[index] : colorsDark[index],
-          percent: percentageRespectTo(budgetMetric[0].paymentsOnChain.value, allMetrics.budget),
-        };
-      }),
+          return {
+            image: item.image || '/assets/img/default-icon-cards-budget.svg',
+            codePath: item.codePath,
+            title: formatBudgetName(item.name),
+            description: item.description || 'Finances of the core governance constructs described in the Maker Atlas.',
+            href: `${siteRoutes.finances(item.codePath.replace('atlas/', ''))}?year=${year}`,
+            valueDai: budgetMetric[0].paymentsOnChain.value,
+            totalDai: allMetrics.paymentsOnChain,
+            code: item.code,
+            color: isLight ? colorsLight[index] : colorsDark[index],
+            percent: percentageRespectTo(budgetMetric[0].paymentsOnChain.value, allMetrics.budget),
+          };
+        })
+        .sort((a, b) => b.percent - a.percent),
     [allMetrics.budget, allMetrics.paymentsOnChain, budgets, budgetsAnalytics, colorsDark, colorsLight, isLight, year]
   );
   // Check some value affect the total 100%
