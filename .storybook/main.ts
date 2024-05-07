178bc1675ca528c10c0b2cd2495b3860e1896fe6
Yasiel Cabrera <yasiel9506@gmail.com>
2024-05-07 17:18:14 -0600

    feat: fusion color palette �

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
