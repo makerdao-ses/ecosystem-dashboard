d87b26c89644fe4e85f966bae448045c0067dfad
Yasiel Cabrera <yasiel9506@gmail.com>
2024-05-08 09:20:08 -0600

    feat: update colors in the navbar

--- src/stories/containers/Endgame/components/NavigationTabs/NavigationTabs.tsx
+++ src/stories/containers/Endgame/components/NavigationTabs/NavigationTabs.tsx
@@ -1,10 +1,7 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material';
 import Container from '@ses/components/Container/Container';
-import { useThemeContext } from '@ses/core/context/ThemeContext';
-import lightTheme from '@ses/styles/theme/themes';
 import React from 'react';
 import { NavigationTabEnum } from '../../useEndgameContainer';
-import type { WithIsLight } from '@ses/core/utils/typesHelpers';
 
 interface NavigationTabsProps {
   activeTab: NavigationTabEnum;
@@ -12,8 +9,6 @@ interface NavigationTabsProps {
 }
 
 const NavigationTabs: React.FC<NavigationTabsProps> = ({ activeTab, handlePauseUrlUpdate }) => {
-  const { isLight } = useThemeContext();
-
   const handleOnClick = (tab: NavigationTabEnum) => () => {
     if (typeof document !== 'undefined') {
       handlePauseUrlUpdate();
@@ -25,32 +20,28 @@ const NavigationTabs: React.FC<NavigationTabsProps> = ({ activeTab, handlePauseU
 
   return (
     <Sticky>
-      <Wrapper isLight={isLight}>
+      <Wrapper>
         <Container>
-          <Navigation isLight={isLight}>
+          <Navigation>
             <Tab
-              isLight={isLight}
               active={activeTab === NavigationTabEnum.LATESTS_UPDATES}
               onClick={handleOnClick(NavigationTabEnum.LATESTS_UPDATES)}
             >
               Latest Updates
             </Tab>
             <Tab
-              isLight={isLight}
               active={activeTab === NavigationTabEnum.KEY_CHANGES}
               onClick={handleOnClick(NavigationTabEnum.KEY_CHANGES)}
             >
               Key Changes
             </Tab>
             <Tab
-              isLight={isLight}
               active={activeTab === NavigationTabEnum.BUDGET_STRUCTURE}
               onClick={handleOnClick(NavigationTabEnum.BUDGET_STRUCTURE)}
             >
               Endgame Budget Structure
             </Tab>
             <Tab
-              isLight={isLight}
               active={activeTab === NavigationTabEnum.BUDGET_TRANSITION_STATUS}
               onClick={handleOnClick(NavigationTabEnum.BUDGET_TRANSITION_STATUS)}
             >
@@ -65,15 +56,15 @@ const NavigationTabs: React.FC<NavigationTabsProps> = ({ activeTab, handlePauseU
 
 export default NavigationTabs;
 
-const Sticky = styled.div({
+const Sticky = styled('div')({
   position: 'sticky',
   top: 63,
   zIndex: 2,
 });
 
-const Wrapper = styled.div<WithIsLight>(({ isLight }) => ({
-  backgroundColor: isLight ? 'white' : '#000f1d',
-  backdropFilter: isLight ? 'none' : 'blur(20px)',
+const Wrapper = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.isLight ? theme.palette.colors.gray[50] : '#1B1E24',
+  backdropFilter: theme.palette.isLight ? 'none' : 'blur(20px)',
   width: '100%',
   overflowX: 'scroll',
   msOverflowStyle: 'none',
@@ -84,42 +75,50 @@ const Wrapper = styled.div<WithIsLight>(({ isLight }) => ({
   },
 }));
 
-const Navigation = styled.nav<WithIsLight>(({ isLight }) => ({
-  borderBottom: `1px solid ${isLight ? '#B6EDE7' : '#405361'}`,
+const Navigation = styled('nav')(({ theme }) => ({
+  borderBottom: `1px solid ${theme.palette.isLight ? theme.palette.colors.gray[500] : theme.palette.colors.slate[400]}`,
   display: 'flex',
   justifyContent: 'center',
   gap: 16,
   minWidth: 'fit-content',
   width: '100%',
 
-  [lightTheme.breakpoints.down('tablet_768')]: {
+  [theme.breakpoints.down('tablet_768')]: {
     marginLeft: -16,
     paddingLeft: 16,
     paddingRight: 16,
   },
 
-  [lightTheme.breakpoints.up('tablet_768')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     gap: 56,
   },
 }));
 
-const Tab = styled.div<WithIsLight & { active?: boolean }>(({ isLight, active = false }) => {
-  const activeColor = isLight ? '#1AAB9B' : '#2DC1B1';
-  const defaultColor = '#708390';
+const Tab = styled('div')<{ active?: boolean }>(({ theme, active = false }) => {
+  const activeColor = theme.palette.isLight ? theme.palette.colors.gray[900] : theme.palette.colors.slate[50];
+  const defaultColor = theme.palette.isLight ? theme.palette.colors.slate[100] : theme.palette.colors.slate[400];
 
   return {
     fontSize: 14,
+    lineHeight: '22px',
+    fontWeight: 600,
     color: active ? activeColor : defaultColor,
-    paddingBottom: 10,
-    paddingTop: 10,
+    paddingBottom: 6,
+    paddingTop: 6,
     borderBottom: `2px solid ${active ? activeColor : 'transparent'}`,
     whiteSpace: 'nowrap',
     cursor: 'pointer',
 
-    [lightTheme.breakpoints.up('tablet_768')]: {
-      fontSize: 16,
+    [theme.breakpoints.up('tablet_768')]: {
+      fontSize: 14,
       lineHeight: '22px',
       paddingBottom: 12,
+      paddingTop: 12,
+    },
+
+    [theme.breakpoints.up('desktop_1024')]: {
+      fontSize: 16,
+      lineHeight: '24px',
     },
   };
 });
