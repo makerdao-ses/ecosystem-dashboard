f5a4f24be148db09819f0960950d479843ecbd73
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-05-20 15:02:06 -0300

    Fix the stories and fix format in mobile

--- src/views/Endgame/components/BudgetTransitionStatusSection/BudgetTransitionStatusSection.stories.tsx
+++ src/views/Endgame/components/BudgetTransitionStatusSection/BudgetTransitionStatusSection.stories.tsx
@@ -8,50 +8,67 @@ const meta: Meta<typeof BudgetTransitionStatusSection> = {
   component: BudgetTransitionStatusSection,
   parameters: {
     chromatic: {
-      viewports: [375, 768, 1024, 1280, 1440],
       pauseAnimationAtEnd: true,
     },
   },
 };
 export default meta;
-
+const data = {
+  '2021/Q1': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2021/Q2': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2021/Q3': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2021/Q4': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2022/Q1': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2022/Q2': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2022/Q3': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2022/Q4': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2023/Q1': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2023/Q2': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2023/Q3': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+  '2023/Q4': {
+    legacy: 103289909.62499996,
+    endgame: 36306730.765775874,
+  },
+};
 const variantsArgs = [
   {
     selected: 'Budget',
     handleChange: () => null,
     data: {
-      '2022/Q1': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2022/Q2': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2022/Q3': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2022/Q4': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2023/Q1': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2023/Q2': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2023/Q3': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
-      '2023/Q4': {
-        legacy: 103289909.62499996,
-        endgame: 36306730.765775874,
-      },
+      ...data,
       '2024/Q1': {
         legacy: 103289909.62499996,
         endgame: 36306730.765775874,
@@ -70,27 +87,25 @@ const variantsArgs = [
       },
     },
   },
+  {
+    selected: 'Budget',
+    handleChange: () => null,
+    data,
+  },
 ];
 
-const [[LightMode, DarkMode]] = createThemeModeVariants(BudgetTransitionStatusSection, variantsArgs);
-export { LightMode, DarkMode };
+const [[LightModeMobile, DarkModeMobile], [LightMode, DarkMode]] = createThemeModeVariants(
+  BudgetTransitionStatusSection,
+  variantsArgs
+);
+export { LightModeMobile, DarkModeMobile, LightMode, DarkMode };
 
 LightMode.parameters = {
+  chromatic: {
+    viewports: [768, 1024, 1280, 1440],
+  },
   figma: {
     component: {
-      375: {
-        component:
-          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=219:51528&t=CMVqLVlvnrm0JRjr-4',
-        options: {
-          componentStyle: {
-            width: 343,
-          },
-          style: {
-            top: -12,
-            left: -14,
-          },
-        },
-      },
       768: {
         component:
           'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=208:27252&t=CMVqLVlvnrm0JRjr-4',
@@ -146,3 +161,26 @@ LightMode.parameters = {
     },
   } as FigmaParams,
 };
+LightModeMobile.parameters = {
+  chromatic: {
+    viewports: [375],
+  },
+  figma: {
+    component: {
+      375: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=219:51528&t=CMVqLVlvnrm0JRjr-4',
+        options: {
+          componentStyle: {
+            width: 343,
+          },
+          style: {
+            top: -12,
+            left: -14,
+          },
+        },
+      },
+    },
+  } as FigmaParams,
+};
+DarkModeMobile.parameters = {};
