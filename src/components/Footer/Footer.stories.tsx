4fa5ce4ffea562ccbe0f5f971afdc1136bc3c8d6
Yoelvys Perez Cabrera <yoelvys.perez@dspot.pl.com>
2024-05-24 11:35:29 -0400

    [Reskin-435] update of footer links

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -17,7 +17,76 @@ const meta: Meta<typeof Footer> = {
 };
 export default meta;
 
-const args = undefined;
+const args = [
+  {
+    linkCategory: [
+      {
+        name: 'Governance',
+        links: [
+          {
+            label: 'Maker forum',
+            link: 'https://forum.makerdao.com/',
+          },
+          {
+            label: 'Voting portal',
+            link: 'https://vote.makerdao.com/',
+          },
+          {
+            label: 'MIPs portal',
+            link: 'https://mips.makerdao.com/mips/lis',
+          },
+          {
+            // eslint-disable-next-line spellcheck/spell-checker
+            label: 'Makerburn vnext',
+            link: 'https://makerburn.com/v2',
+          },
+        ],
+      },
+      {
+        name: 'Documentation',
+        links: [
+          {
+            label: 'Organization',
+            link: 'https://www.powerhouse.inc/',
+          },
+          {
+            label: 'Technical Docs',
+            link: 'https://powerhouse-1.gitbook.io/powerhouse',
+          },
+          {
+            label: 'Brand Assets',
+            link: '#',
+          },
+          {
+            label: 'Github Repos',
+            link: 'https://github.com/powerhouse-inc',
+          },
+        ],
+      },
+      {
+        name: 'MakerDao Tools',
+        links: [
+          {
+            label: 'Connect',
+            link: '#',
+            icon: '/assets/img/footer/footer_link_connect.svg',
+          },
+          {
+            label: 'Switchboard',
+            link: 'https://powerhouse-nginx-router-5dcc24a23f9e.herokuapp.com/makerdao/switchboard',
+            icon: '/assets/img/footer/footer_link_switchboard.svg',
+          },
+          {
+            label: 'Fusion',
+            link: '#',
+            icon: '/assets/img/footer/footer_link_fusion.svg',
+          },
+        ],
+      },
+    ],
+  },
+];
+
 const [[LightMode, DarkMode]] = createThemeModeVariants(Footer, args, false);
 export { LightMode, DarkMode };
 
