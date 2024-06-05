d1b89e11deb5b5b9594c3720aaf61c0f4cdf4677
Yasiel Cabrera <yasiel9506@gmail.com>
2024-06-05 17:12:53 -0400

    feat: dockerize app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /* eslint-disable spellcheck/spell-checker */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   compiler: {
     emotion: true,
   },
-  webpack (config) {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
