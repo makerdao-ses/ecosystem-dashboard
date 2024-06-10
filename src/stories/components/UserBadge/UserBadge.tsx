a646a1d7b6ca8e0a06c47e5dc4c62b6be441e55e
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-10 17:50:48 -0300

    Fix avatar in UserBadge

--- src/stories/components/UserBadge/UserBadge.tsx
+++ src/stories/components/UserBadge/UserBadge.tsx
@@ -1,57 +1,37 @@
-import styled from '@emotion/styled';
-import { useMediaQuery } from '@mui/material';
+import { styled, useMediaQuery } from '@mui/material';
 import React from 'react';
-import { useThemeContext } from '../../../core/context/ThemeContext';
 import { CircleAvatar } from '../CircleAvatar/CircleAvatar';
 
 interface Props {
   username?: string;
-  style?: React.CSSProperties;
+
   onClick?: () => void;
 }
 
-const UserBadge: React.FC<Props> = ({ onClick, username, style }) => {
+const UserBadge: React.FC<Props> = ({ onClick, username }) => {
   const isDesktop = useMediaQuery('(min-width: 834px)');
-  const { isLight } = useThemeContext();
 
   return isDesktop ? (
-    <Container isLight={isLight} className="no-select" style={style} onClick={onClick}>
-      <CircleAvatar
-        width="32px"
-        height="32px"
-        name={username ?? 'Username'}
-        fontSize="14px"
-        style={{
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          border: '2px solid #708390',
-        }}
-      />
-      <UserName isLight={isLight}>{username ?? 'Username'}</UserName>
+    <Container className="no-select" style={style} onClick={onClick}>
+      <CircleAvatarStyledWithName width="32px" height="32px" name={username ?? 'Username'} fontSize="14px" />
+      <UserName>{username ?? 'Username'}</UserName>
     </Container>
   ) : (
-    <CircleAvatar
+    <CircleAvatarStyledWithoutName
       width="35px"
       height="35px"
       name={username ?? 'Username'}
       fontSize="14px"
       onClick={onClick}
-      style={{
-        border: `1px solid ${isLight ? '#D4D9E1' : '#708390'}`,
-        cursor: 'pointer',
-        marginRight: 16,
-        ...style,
-      }}
     />
   );
 };
 
 export default UserBadge;
 
-const Container = styled.div<{ isLight: boolean }>(({ isLight }) => ({
-  background: isLight ? '#F6F8F9' : '#1E2C37',
-  border: isLight ? '1px solid #D4D9E1' : '1px solid #343442',
+const Container = styled('div')(({ theme }) => ({
+  background: theme.palette.isLight ? '#F6F8F9' : '#1E2C37',
+  border: theme.palette.isLight ? '1px solid #D4D9E1' : '1px solid #343442',
   borderRadius: 22,
   padding: ' 7px 16px 7px 40px',
   position: 'relative',
@@ -60,9 +40,22 @@ const Container = styled.div<{ isLight: boolean }>(({ isLight }) => ({
   cursor: 'pointer',
 }));
 
-const UserName = styled.div<{ isLight: boolean }>(({ isLight }) => ({
-  color: isLight ? '#231536' : '#D2D4EF',
+const UserName = styled('div')(({ theme }) => ({
+  color: theme.palette.isLight ? '#231536' : '#D2D4EF',
   fontWeight: 500,
   fontSize: 14,
   lineHeight: '18px',
+}));
+
+const CircleAvatarStyledWithName = styled(CircleAvatar)({
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  border: '2px solid #708390',
+});
+
+const CircleAvatarStyledWithoutName = styled(CircleAvatar)(({ theme }) => ({
+  border: `1px solid ${theme.palette.isLight ? '#D4D9E1' : '#708390'}`,
+  cursor: 'pointer',
+  marginRight: 16,
 }));
