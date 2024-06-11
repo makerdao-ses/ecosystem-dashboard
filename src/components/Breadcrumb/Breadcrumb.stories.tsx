93e62e24fa9dceaa9a07c68109c27a9e305df133
Yasiel Cabrera <yasiel9506@gmail.com>
2024-06-10 20:29:32 -0400

    feat: complete breadcrumb stories

--- src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -4,6 +4,7 @@ import { withoutSBPadding } from '@/core/utils/storybook/decorators';
 import Breadcrumb from './Breadcrumb';
 import TeamBreadcrumbContent from './CustomContents/TeamBreadcrumbContent';
 import type { Meta } from '@storybook/react';
+import type { FigmaParams } from 'sb-figma-comparator';
 
 const meta: Meta<typeof Breadcrumb> = {
   title: 'Fusion/Components/Breadcrumb',
@@ -54,33 +55,20 @@ const variantsArgs = [
   {
     items: [
       {
-        label: '1- Home',
+        label: 'Finances',
         href: '/',
-        number: 15,
       },
       {
-        label: '2.1- Lorem ipsum',
-        href: '/a1',
+        label: 'Something else',
+        href: '/something-else',
       },
       {
-        label: '2.2- Lorem ipsum consectetur',
-        href: '/a1',
+        label: 'Atlas Immutable Budget',
+        href: '/atlas-immutable-budget',
       },
       {
-        label: '2.3- Lorem ipsum lorem ipsum',
-        href: '/a1',
-      },
-      {
-        label: '3- Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        href: '/a2',
-      },
-      {
-        label: '4- Lorem ipsum dolor',
-        href: '/a3',
-      },
-      {
-        label: '5- A very long text that should be truncated',
-        href: '/long',
+        label: 'Generative Tokens',
+        href: '/generative-tokens',
       },
     ],
     rightContent,
@@ -93,3 +81,40 @@ const [[LightModeDefault, DarkModeDefault], [WithLongLabelsLight, WithLongLabels
   false
 );
 export { LightModeDefault, DarkModeDefault, WithLongLabelsLight, WithLongLabelsDark };
+
+WithLongLabelsLight.parameters = {
+  figma: {
+    component: {
+      375: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2293-28769&t=kzkccrF7VEewOmD3-4',
+        options: {
+          style: {
+            top: -20,
+            left: -3,
+          },
+        },
+      },
+      768: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2293-28522&t=kzkccrF7VEewOmD3-4',
+        options: {
+          style: {
+            top: -20,
+            left: -19,
+          },
+        },
+      },
+      1024: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2293-28419&t=kzkccrF7VEewOmD3-4',
+        options: {
+          style: {
+            top: -19,
+            left: -19,
+          },
+        },
+      },
+    },
+  } as FigmaParams,
+};
