c6de39b855f62f64b1f9c1c1958ecd9e4bb24928
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-19 18:04:10 -0300

    Fix the small stories for project and add auditor stories

---
+++ src/views/EAAbout/components/CardProjects/CustomSheetProjects.stories.tsx
@@ -0,0 +1,42 @@
+import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
+import ItemCustomProject from './ItemCustomProject';
+import type { Meta } from '@storybook/react';
+
+const meta: Meta<typeof ItemCustomProject> = {
+  title: 'Fusion/ActorAbout/ItemCustomProject',
+  component: ItemCustomProject,
+
+  parameters: {
+    chromatic: {
+      viewports: [375],
+      pauseAnimationAtEnd: true,
+    },
+  },
+};
+export default meta;
+
+const variantsArgs = [
+  {
+    shortCode: 'Powerhouse',
+  },
+];
+
+const [[LightMode, DarkMode]] = createThemeModeVariants(ItemCustomProject, variantsArgs);
+export { LightMode, DarkMode };
+
+LightMode.parameters = {
+  figma: {
+    component: {
+      375: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=1317:53357&t=OVqwgbDObxaThSlv-4',
+        options: {
+          style: {
+            top: 0,
+            left: 0,
+          },
+        },
+      },
+    },
+  },
+};
---
+++ src/views/EAAbout/components/CardProjects/ItemCustomProject.tsx
@@ -0,0 +1,36 @@
+import { styled } from '@mui/material';
+import React from 'react';
+import CardSheetMobile from '@/components/CardSheetMobile/CardSheetMobile';
+import InternalLinkButton from '@/components/InternalLinkButton/InternalLinkButton';
+import { siteRoutes } from '@/config/routes';
+import type { FC } from 'react';
+
+interface Props {
+  shortCode: string;
+}
+
+const ItemCustomProject: FC<Props> = ({ shortCode }) => {
+  console.log('hello');
+  return (
+    <CardSheetMobile
+      title="Projects"
+      description={`View all the of the projects ${shortCode} is involved in and there status.`}
+    >
+      <ContainerChildren>
+        <InternalLinkButtonStyled href={siteRoutes.ecosystemActorProjects(shortCode)} label="View Projects" showIcon />
+      </ContainerChildren>
+    </CardSheetMobile>
+  );
+};
+
+export default ItemCustomProject;
+
+const ContainerChildren = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 8,
+});
+
+const InternalLinkButtonStyled = styled(InternalLinkButton)(() => ({
+  padding: '4px 13.5px 4px 13.5px',
+}));
