48ef6a1be01f33d94b2274dd248d8336d6ef733c
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-25 15:16:15 -0300

    Fix the legacy breakpoints

--- src/stories/containers/CUActivity/CUActivityFeedContainer.tsx
+++ src/stories/containers/CUActivity/CUActivityFeedContainer.tsx
@@ -1,8 +1,7 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material';
 import { siteRoutes } from '@ses/config/routes';
 import { useHeaderSummary } from '@ses/core/hooks/useHeaderSummary';
 import React from 'react';
-import lightTheme from '../../../../styles/theme/themes';
 import { toAbsoluteURL } from '../../../core/utils/urls';
 import ActivityTable from '../../components/CUActivityTable/ActivityTable';
 import { CoreUnitSummary } from '../../components/CoreUnitSummary/CoreUnitSummary';
@@ -18,7 +17,7 @@ interface CUActivityContainerProps {
 }
 
 const CUActivityFeedContainer: React.FC<CUActivityContainerProps> = ({ coreUnit, coreUnits, activities }) => {
-  const { isLight, columns, onSortClick, code, ref } = useCuActivity();
+  const { columns, onSortClick, code, ref } = useCuActivity();
 
   const { height, showHeader } = useHeaderSummary(ref, code);
   return (
@@ -37,7 +36,7 @@ const CUActivityFeedContainer: React.FC<CUActivityContainerProps> = ({ coreUnit,
         ref={ref}
         showHeader={showHeader}
       />
-      <Container isLight={isLight}>
+      <Container>
         <InnerPage marginTop={height}>
           <TableWrapper>
             <ActivityTable
@@ -49,8 +48,8 @@ const CUActivityFeedContainer: React.FC<CUActivityContainerProps> = ({ coreUnit,
               sortClick={onSortClick}
             />
           </TableWrapper>
-          <Title isLight={isLight}>Additional Notes</Title>
-          <Paragraph isLight={isLight}>
+          <Title>Additional Notes</Title>
+          <Paragraph>
             The table below reflects the activity regarding the {coreUnit.shortCode} Core Unit. Here you will be able to
             see all previous modifications that the Core Unit has made to their Expense Reports, FTEs, and more.
           </Paragraph>
@@ -62,29 +61,29 @@ const CUActivityFeedContainer: React.FC<CUActivityContainerProps> = ({ coreUnit,
 
 export default CUActivityFeedContainer;
 
-const Wrapper = styled.div({
+const Wrapper = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   width: '100%',
 });
 
-const Container = styled.div<{ isLight: boolean }>(({ isLight }) => ({
+const Container = styled('div')(({ theme }) => ({
   flexDirection: 'column',
   alignItems: 'center',
   paddingTop: '64px',
   flex: 1,
-  backgroundColor: isLight ? '#FFFFFF' : '#000000',
-  backgroundImage: isLight ? 'url(/assets/img/bg-page.png)' : 'url(/assets/img/bg-page-dark.png)',
+  backgroundColor: theme.palette.isLight ? '#FFFFFF' : '#000000',
+  backgroundImage: theme.palette.isLight ? 'url(/assets/img/bg-page.png)' : 'url(/assets/img/bg-page-dark.png)',
   backgroundAttachment: 'fixed',
   backgroundSize: 'cover',
   paddingBottom: '79px',
 
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     paddingBottom: '128px',
   },
 }));
 
-const InnerPage = styled.div<{ marginTop: number }>(({ marginTop }) => ({
+const InnerPage = styled('div')<{ marginTop: number }>(({ marginTop, theme }) => ({
   display: 'block',
   textAlign: 'left',
   width: '100%',
@@ -95,45 +94,45 @@ const InnerPage = styled.div<{ marginTop: number }>(({ marginTop }) => ({
   paddingRight: '64px',
   paddingLeft: '64px',
 
-  [lightTheme.breakpoints.up('desktop_1920')]: {
+  [theme.breakpoints.up('desktop_1920')]: {
     maxWidth: '1312px',
     paddingRight: '0px',
     paddingLeft: '0px',
   },
-  [lightTheme.breakpoints.between('desktop_1280', 'desktop_1440')]: {
+  [theme.breakpoints.between('desktop_1280', 'desktop_1440')]: {
     paddingRight: '48px',
     paddingLeft: '48px',
   },
-  [lightTheme.breakpoints.between('table_834', 'desktop_1280')]: {
+  [theme.breakpoints.between('tablet_768', 'desktop_1280')]: {
     paddingRight: '32px',
     paddingLeft: '32px',
   },
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     paddingTop: 32,
   },
-  [lightTheme.breakpoints.down('table_834')]: {
+  [theme.breakpoints.down('tablet_768')]: {
     paddingRight: '16px',
     paddingLeft: '16px',
   },
 }));
 
-export const Title = styled.div<{
+export const Title = styled('div')<{
   marginBottom?: number;
-  isLight: boolean;
+
   fontSize?: string;
   responsiveMarginBottom?: number;
-}>(({ marginBottom = 16, fontSize = '20px', isLight, responsiveMarginBottom }) => ({
+}>(({ marginBottom = 16, fontSize = '20px', responsiveMarginBottom, theme }) => ({
   fontFamily: 'Inter, sans-serif',
   fontWeight: 700,
   fontStyle: 'normal',
   fontSize,
   lineHeight: '19px',
   letterSpacing: '0.4px',
-  color: isLight ? '#231536' : '#D2D4EF',
+  color: theme.palette.isLight ? '#231536' : '#D2D4EF',
   marginBottom,
   marginTop: 64,
 
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     fontSize: '24px',
     fontWeight: 600,
     lineHeight: '24px',
@@ -141,22 +140,22 @@ export const Title = styled.div<{
   },
 }));
 
-export const Paragraph = styled.p<{ isLight: boolean }>(({ isLight }) => ({
+export const Paragraph = styled('p')(({ theme }) => ({
   fontFamily: 'Inter, sans-serif',
   fontStyle: 'normal',
   fontWeight: 400,
   fontSize: '14px',
   lineHeight: '22px',
-  color: isLight ? '#231536' : '#D2D4EF',
+  color: theme.palette.isLight ? '#231536' : '#D2D4EF',
   marginBottom: 0,
 
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     fontSize: '16px',
   },
 }));
 
-const TableWrapper = styled.div({
+const TableWrapper = styled('div')({
   maxWidth: '928px',
   width: '100%',
-  margin: '0 auto',
+  margin: '0px auto',
 });
