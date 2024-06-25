8ad5c62241fca6d1ea4ed0aa2ee0092c4e727c19
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-25 15:40:08 -0300

    Fix the container of delegate report

--- src/stories/containers/RecognizedDelegatesReports/RecognizedDelegatesReportContainer.tsx
+++ src/stories/containers/RecognizedDelegatesReports/RecognizedDelegatesReportContainer.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material';
 import { CustomLink } from '@ses/components/CustomLink/CustomLink';
 import { CustomPager } from '@ses/components/CustomPager/CustomPager';
 import DelegateSummary from '@ses/components/DelegateSummary/DelegateSummary';
@@ -10,7 +10,6 @@ import { useHeaderSummary } from '@ses/core/hooks/useHeaderSummary';
 import { BudgetStatus } from '@ses/core/models/dto/coreUnitDTO';
 import { ResourceType } from '@ses/core/models/interfaces/types';
 import { toAbsoluteURL } from '@ses/core/utils/urls';
-import lightTheme from '@ses/styles/theme/themes';
 import React, { useRef } from 'react';
 import ExpenseReportStatusIndicator from '../TransparencyReport/components/ExpenseReportStatusIndicator/ExpenseReportStatusIndicator';
 import AuditorCommentsContainer from '../TransparencyReport/components/TransparencyAuditorComments/AuditorCommentsContainer/AuditorCommentsContainer';
@@ -115,8 +114,8 @@ const RecognizedDelegatesReportContainer: React.FC<RecognizedDelegatesProps> = (
         )}
 
         <ContainerAdditionalNotes>
-          <TitleNotes isLight={isLight}>Additional Notes</TitleNotes>
-          <Description isLight={isLight}>
+          <TitleNotes>Additional Notes</TitleNotes>
+          <Description>
             The expenses on this page are for all Recognized Delegates that receive compensation. To view all delegates
             visit
             <span>
@@ -129,7 +128,7 @@ const RecognizedDelegatesReportContainer: React.FC<RecognizedDelegatesProps> = (
               />
             </span>
           </Description>
-          <Description isLight={isLight}>
+          <Description>
             MakerDAO forum reports for delegate can be found
             <SpacerDescription style={{ width: 1 }} />
             <CustomLink
@@ -151,16 +150,16 @@ const RecognizedDelegatesReportContainer: React.FC<RecognizedDelegatesProps> = (
 
 export default RecognizedDelegatesReportContainer;
 
-const Container = styled.div<{ isLight: boolean }>(({ isLight }) => ({
+const Container = styled('div')<{ isLight: boolean }>(({ theme }) => ({
   paddingTop: 64,
   width: '100%',
-  backgroundColor: isLight ? '#FFFFFF' : '#000000',
-  backgroundImage: isLight ? 'url(/assets/img/bg-page.png)' : 'url(/assets/img/bg-page-dark.png)',
+  backgroundColor: theme.palette.isLight ? '#FFFFFF' : '#000000',
+  backgroundImage: theme.palette.isLight ? 'url(/assets/img/bg-page.png)' : 'url(/assets/img/bg-page-dark.png)',
   backgroundAttachment: 'fixed',
   backgroundSize: 'cover',
 }));
 
-const ContainerInside = styled.div<{ marginTop: number }>(({ marginTop }) => ({
+const ContainerInside = styled('div')<{ marginTop: number }>(({ marginTop, theme }) => ({
   display: 'block',
   textAlign: 'left',
   width: '100%',
@@ -171,102 +170,102 @@ const ContainerInside = styled.div<{ marginTop: number }>(({ marginTop }) => ({
   marginTop: 22 + marginTop,
   paddingRight: '64px',
   paddingLeft: '64px',
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     marginTop: 45 + marginTop,
   },
 
-  [lightTheme.breakpoints.up('desktop_1194')]: {
+  [theme.breakpoints.up('desktop_1024')]: {
     marginTop: 40 + marginTop,
   },
 
-  [lightTheme.breakpoints.up('desktop_1920')]: {
+  [theme.breakpoints.up('desktop_1920')]: {
     maxWidth: '1312px',
     paddingRight: '0px',
     paddingLeft: '0px',
   },
 
-  [lightTheme.breakpoints.between('desktop_1280', 'desktop_1440')]: {
+  [theme.breakpoints.between('desktop_1280', 'desktop_1440')]: {
     paddingRight: '48px',
     paddingLeft: '48px',
   },
 
-  [lightTheme.breakpoints.between('table_834', 'desktop_1280')]: {
+  [theme.breakpoints.between('tablet_768', 'desktop_1280')]: {
     paddingRight: '32px',
     paddingLeft: '32px',
   },
 
-  [lightTheme.breakpoints.down('table_834')]: {
+  [theme.breakpoints.down('tablet_768')]: {
     paddingRight: '16px',
     paddingLeft: '16px',
   },
 }));
 
-const ContainerPagerBar = styled.div({
-  [lightTheme.breakpoints.up('table_834')]: {
+const ContainerPagerBar = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('tablet_768')]: {
     marginTop: -3,
   },
-});
+}));
 
-const StyledPagerBar = styled(CustomPager)({
+const StyledPagerBar = styled(CustomPager)(({ theme }) => ({
   '&.styledPagerBar': {
     'div:first-of-type': {
       gap: 24,
-      [lightTheme.breakpoints.up('table_834')]: {
+      [theme.breakpoints.up('tablet_768')]: {
         gap: 8,
       },
     },
     '> div:last-of-type': {
       marginLeft: 8,
       letterSpacing: 0,
-      [lightTheme.breakpoints.up('table_834')]: {
+      [theme.breakpoints.up('tablet_768')]: {
         letterSpacing: ' 0.4px',
       },
     },
   },
-});
+}));
 
-const PagerBar = styled.div({
+const PagerBar = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'flex-start',
   flex: 1,
   marginTop: 2,
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     alignItems: 'center',
     height: '34px',
     marginTop: 4,
     marginLeft: 2,
   },
-});
+}));
 
-const PagerBarLeft = styled.div({
+const PagerBarLeft = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'flex-start',
   flexDirection: 'column',
 
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     alignItems: 'center',
     flexDirection: 'row',
   },
-});
+}));
 
-const Spacer = styled.div({
+const Spacer = styled('div')({
   flex: '1',
 });
 
-const LastUpdate = styled.div({
+const LastUpdate = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'flex-end',
   fontFamily: 'Inter, sans-serif',
   marginTop: -3,
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     marginTop: 0,
     marginRight: -2,
     marginLeft: 8,
   },
-});
+}));
 
-const Since = styled.div<{ isLight: boolean }>(({ isLight = true }) => ({
+const Since = styled('div')<{ isLight: boolean }>(({ isLight = true }) => ({
   color: isLight ? '#231536' : '#D2D4EF',
   fontSize: '11px',
   lineHeight: '15px',
@@ -281,7 +280,7 @@ const Since = styled.div<{ isLight: boolean }>(({ isLight = true }) => ({
   },
 }));
 
-const SinceDate = styled.div({
+const SinceDate = styled('div')({
   color: '#708390',
   fontFamily: 'Inter, sans-serif',
   fontSize: '11px',
@@ -298,32 +297,32 @@ const SinceDate = styled.div({
   },
 });
 
-const ContainerTabs = styled.div({
+const ContainerTabs = styled('div')(({ theme }) => ({
   margin: '32px 0',
 
   '& > div > div > a': {
     paddingBottom: 8,
 
-    [lightTheme.breakpoints.up('table_834')]: {
+    [theme.breakpoints.up('tablet_768')]: {
       paddingBottom: 14,
     },
   },
-});
+}));
 
-const ContainerExpense = styled.div({
+const ContainerExpense = styled('div')(({ theme }) => ({
   marginTop: -2,
   'div a': {
     marginLeft: 4,
-    [lightTheme.breakpoints.up('table_834')]: {
+    [theme.breakpoints.up('tablet_768')]: {
       marginLeft: 8,
     },
   },
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     marginLeft: -3,
   },
-});
+}));
 
-const ContainerAdditionalNotes = styled.div({
+const ContainerAdditionalNotes = styled('div')({
   marginTop: 40,
   marginBottom: 64,
   fontFamily: 'Inter, sans-serif',
@@ -333,12 +332,12 @@ const ContainerAdditionalNotes = styled.div({
   gap: 16,
 });
 
-const Description = styled.div<{ isLight?: boolean }>(({ isLight }) => ({
+const Description = styled('div')(({ theme }) => ({
   fontWeight: 400,
   fontSize: 14,
   lineHeight: '22px',
   letterSpacing: 0,
-  color: isLight ? '#231536' : '#d2d4ef',
+  color: theme.palette.isLight ? '#231536' : '#d2d4ef',
   '> a': {
     fontSize: 14,
     lineHeight: '18px',
@@ -349,7 +348,7 @@ const Description = styled.div<{ isLight?: boolean }>(({ isLight }) => ({
     lineHeight: '18px',
     letterSpacing: 0,
   },
-  [lightTheme.breakpoints.up('table_834')]: {
+  [theme.breakpoints.up('tablet_768')]: {
     fontSize: 16,
     '> a': {
       fontSize: 16,
@@ -363,20 +362,20 @@ const Description = styled.div<{ isLight?: boolean }>(({ isLight }) => ({
     },
   },
 }));
-const TitleNotes = styled.div<{ isLight?: boolean }>(({ isLight }) => ({
+const TitleNotes = styled('div')(({ theme }) => ({
   fontWeight: 700,
   fontSize: '16px',
   lineHeight: '19px',
   display: 'flex',
-  color: isLight ? '#231536' : '#d2d4ef',
-  [lightTheme.breakpoints.up('table_834')]: {
+  color: theme.palette.isLight ? '#231536' : '#d2d4ef',
+  [theme.breakpoints.up('tablet_768')]: {
     fontSize: 20,
     lineHeight: '24px',
   },
 }));
 
-const SpacerDescription = styled.div({
-  [lightTheme.breakpoints.up('table_834')]: {
+const SpacerDescription = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('tablet_768')]: {
     display: 'none',
   },
-});
+}));
