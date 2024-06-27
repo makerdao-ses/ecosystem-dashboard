2dfe4aae74aa3fe275404a1c083cfc8f5cc6a14c
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-27 12:37:17 -0300

    Fix top-bar stories for all breakpoints

--- src/components/TopBarNavigation/TopBarNavigation.stories.tsx
+++ src/components/TopBarNavigation/TopBarNavigation.stories.tsx
@@ -10,7 +10,13 @@ const meta: Meta<typeof TopBarNavigation> = {
   decorators: [withoutSBPadding],
   parameters: {
     chromatic: {
+      viewports: [375, 768, 1024, 1280, 1920, 1440],
       pauseAnimationAtEnd: true,
+    },
+    nextjs: {
+      router: {
+        pathname: '/contributors',
+      },
     },
   },
 };
@@ -24,3 +30,88 @@ const [[LightMode, DarkMode]] = createThemeModeVariants(
   variantsArgs
 );
 export { LightMode, DarkMode };
+
+LightMode.parameters = {
+  figma: {
+    component: {
+      375: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=142:229&t=pe1fVI3DderPnVw1-4',
+        options: {
+          style: {
+            left: -30,
+            top: -28,
+          },
+          componentStyle: {
+            width: 343,
+          },
+        },
+      },
+      768: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2:1415&t=ajLTBo9VnKC0lToE-4',
+        options: {
+          style: {
+            left: 0,
+            top: -10,
+          },
+          componentStyle: {
+            width: 724,
+          },
+        },
+      },
+      1024: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2:1417&t=pe1fVI3DderPnVw1-4',
+        options: {
+          style: {
+            left: 2,
+            top: -10,
+          },
+          componentStyle: {
+            width: 980,
+          },
+        },
+      },
+      1280: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2:1418&t=FaDSDTCuQY6aC62v-4',
+        options: {
+          style: {
+            left: 8,
+            top: -10,
+          },
+          componentStyle: {
+            width: 1220,
+          },
+        },
+      },
+      1440: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=2:1419&t=FaDSDTCuQY6aC62v-4',
+        options: {
+          style: {
+            left: 32,
+            top: -10,
+          },
+          componentStyle: {
+            width: 1332,
+          },
+        },
+      },
+      1920: {
+        component:
+          'https://www.figma.com/design/iLyzLutlWLu6Yf8tFdlM6T/Fusion%2FPowerhouse?node-id=1996:36323&t=ajLTBo9VnKC0lToE-4',
+        options: {
+          style: {
+            left: 4,
+            top: -10,
+          },
+          componentStyle: {
+            width: 1876,
+          },
+        },
+      },
+    },
+  },
+};
