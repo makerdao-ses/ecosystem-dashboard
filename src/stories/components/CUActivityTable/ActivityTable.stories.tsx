536761e7cb235a4a7a941932d474bad97824c919
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-06-27 11:39:54 -0300

    Fx activity storie

--- src/stories/components/CUActivityTable/ActivityTable.stories.tsx
+++ src/stories/components/CUActivityTable/ActivityTable.stories.tsx
@@ -108,12 +108,12 @@ const args = [
       {
         header: 'Timestamp',
         styles: {
-          [lightTheme.breakpoints.up('table_834')]: {
+          [lightTheme.breakpoints.up('tablet_768')]: {
             width: 262,
             paddingLeft: 32,
             paddingRight: 14,
           },
-          [lightTheme.breakpoints.up('desktop_1194')]: {
+          [lightTheme.breakpoints.up('desktop_1024')]: {
             width: 339,
             paddingLeft: 64,
             paddingRight: 14,
