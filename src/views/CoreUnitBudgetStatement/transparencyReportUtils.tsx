4ba5f22b246668d612fdde64511b31ee168db2a7
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-07-03 10:23:45 -0300

    Add styles for the modal

--- src/views/CoreUnitBudgetStatement/transparencyReportUtils.tsx
+++ src/views/CoreUnitBudgetStatement/transparencyReportUtils.tsx
@@ -1,8 +1,7 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { CustomPopover } from '@ses/components/CustomPopover/CustomPopover';
 import IconOpenModal from '@ses/components/svg/IconOpenModal';
-import { useThemeContext } from '@ses/core/context/ThemeContext';
 import { zIndexEnum } from '@ses/core/enums/zIndexEnum';
 import { useScrollLock } from '@ses/core/hooks/useScrollLock';
 import { getPageWrapper } from '@ses/core/utils/dom';
@@ -68,8 +67,7 @@ export const renderLinksWithToken = (address: string) => (
   </TextCell>
 );
 
-export interface WithIsLightAndClick {
-  isLight: boolean;
+export interface WithClick {
   onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
@@ -80,8 +78,8 @@ export const RenderNumberWithIcon = (data: TargetBalanceTooltipInformation) => {
   const handleShowPopoverWhenNotSpace = (value: boolean) => {
     setMarginTopPopoverPosition(value);
   };
-  const { isLight } = useThemeContext();
-  const isMobileResolution = useMediaQuery(lightTheme.breakpoints.down('table_834'));
+
+  const isMobileResolution = useMediaQuery(lightTheme.breakpoints.down('tablet_768'));
   const { lockScroll, unlockScroll } = useScrollLock();
   const showIconToolTip = !!(data.description && data.link);
 
@@ -240,19 +238,19 @@ export const RenderNumberWithIcon = (data: TargetBalanceTooltipInformation) => {
           />
         </ModalSheet>
       )}
-      {isMobileResolution && isOpen && isMobileDevice && <ContainerOverlay isLight={isLight} onClick={handleOnclose} />}
+      {isMobileResolution && isOpen && isMobileDevice && <ContainerOverlay onClick={handleOnclose} />}
     </BiggerContainer>
   );
 };
 
-const ContainerOverlay = styled.div<WithIsLightAndClick>(({ isLight, onClick }) => ({
+const ContainerOverlay = styled('div')<WithClick>(({ onClick, theme }) => ({
   position: 'fixed',
   top: 0,
   left: 0,
   width: '100%',
   height: '100%',
-  background: isLight ? 'rgba(52, 52, 66, 0.1)' : 'rgba(0, 22, 78, 0.1);',
-  backdropFilter: isLight ? 'blur(2px)' : 'blur(4px)',
+  background: theme.palette.isLight ? 'rgba(52, 52, 66, 0.1)' : 'rgba(0, 22, 78, 0.1);',
+  backdropFilter: theme.palette.isLight ? 'blur(2px)' : 'blur(4px)',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
@@ -260,7 +258,7 @@ const ContainerOverlay = styled.div<WithIsLightAndClick>(({ isLight, onClick }) 
   cursor: onClick ? 'default' : undefined,
 }));
 
-const ModalSheet = styled.div({
+const ModalSheet = styled('div')({
   width: '100%',
   zIndex: 5,
   textAlign: 'left',
@@ -270,13 +268,13 @@ const ModalSheet = styled.div({
   right: 0,
 });
 
-const PopoverContainer = styled.div({
+const PopoverContainer = styled('div')({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
   flex: 1,
 });
-const Container = styled.div({
+const Container = styled('div')({
   width: '100%',
   display: 'flex',
   flex: 1,
@@ -284,7 +282,7 @@ const Container = styled.div({
   alignItems: 'center',
   justifyContent: 'space-between',
   marginTop: -8,
-  [lightTheme.breakpoints.up('table_834')]: {
+  [lightTheme.breakpoints.up('tablet_768')]: {
     width: '100%',
     flexDirection: 'row-reverse',
     marginLeft: 0,
@@ -292,7 +290,7 @@ const Container = styled.div({
   },
 });
 
-export const ContainerInfoIcon = styled.div({
+export const ContainerInfoIcon = styled('div')({
   position: 'relative',
 });
 
@@ -300,12 +298,12 @@ const IconPosition = styled(Information)({
   position: 'absolute',
   top: -14,
   left: -14,
-  [lightTheme.breakpoints.up('table_834')]: {
+  [lightTheme.breakpoints.up('tablet_768')]: {
     alignItems: 'center',
     top: -8,
     left: -10,
   },
-  [lightTheme.breakpoints.up('desktop_1194')]: {
+  [lightTheme.breakpoints.up('desktop_1024')]: {
     alignItems: 'center',
 
     top: -8,
@@ -313,17 +311,17 @@ const IconPosition = styled(Information)({
   },
 });
 
-const ContainerInformation = styled.div({
+const ContainerInformation = styled('div')({
   display: 'flex',
   flex: 1,
   flexDirection: 'column',
   alignItems: 'flex-end',
 
-  [lightTheme.breakpoints.between('table_834', 'desktop_1194')]: {
+  [lightTheme.breakpoints.between('tablet_768', 'desktop_1024')]: {
     alignItems: 'flex-end',
     marginRight: 14,
   },
-  [lightTheme.breakpoints.up('desktop_1194')]: {
+  [lightTheme.breakpoints.up('desktop_1024')]: {
     alignItems: 'flex-end',
     marginRight: 14,
   },
@@ -339,40 +337,40 @@ const ContainerNumberCell = styled(NumberCell)({
   },
 });
 
-const ContainerStyleMonths = styled.div({
+const ContainerStyleMonths = styled('div')({
   fontWeight: 400,
   fontSize: '11px',
   lineHeight: '13px',
   color: '#546978',
-  [lightTheme.breakpoints.up('table_834')]: {
+  [lightTheme.breakpoints.up('tablet_768')]: {
     whiteSpace: 'nowrap',
   },
 });
 
-export const TotalTargetBalance = styled.div({
+export const TotalTargetBalance = styled('div')({
   display: 'flex',
   flexDirection: 'row',
   flex: 1,
   justifyContent: 'flex-end',
   textAlign: 'center',
   fontWeight: 700,
-  [lightTheme.breakpoints.up('table_834')]: {
+  [lightTheme.breakpoints.up('tablet_768')]: {
     marginRight: 16,
   },
 });
 
-const BiggerContainer = styled.div({
+const BiggerContainer = styled('div')({
   width: '100%',
 });
 
 const ExtendedCustomPopover = styled(CustomPopover)<{ hasSpacePositionArrow?: boolean }>(
   ({ hasSpacePositionArrow }) => ({
     '& > div': {
-      [lightTheme.breakpoints.between('table_834', 'desktop_1194')]: {
+      [lightTheme.breakpoints.between('tablet_768', 'desktop_1024')]: {
         marginLeft: -45,
         marginTop: 16,
       },
-      [lightTheme.breakpoints.up('desktop_1194')]: {
+      [lightTheme.breakpoints.up('desktop_1024')]: {
         marginLeft: -32,
         marginTop: hasSpacePositionArrow ? -18 : 18,
       },
@@ -389,12 +387,20 @@ interface Props {
 export const OpenModalTransparency: React.FC<Props> = ({ name, handleOpenModal, className }) => (
   <CategoryRowInsideColumn className={className}>
     {name}
-    <IconOpenModal onClick={handleOpenModal} />
+    <IconOpenModalStyled onClick={handleOpenModal} />
   </CategoryRowInsideColumn>
 );
 
-const CategoryRowInsideColumn = styled.div({
+const CategoryRowInsideColumn = styled('div')({
   display: 'flex',
   alignItems: 'center',
   gap: 12,
 });
+
+const IconOpenModalStyled = styled(IconOpenModal)(({ theme }) => ({
+  ':hover': {
+    '& rect': {
+      stroke: theme.palette.isLight ? theme.palette.colors.slate[200] : theme.palette.colors.slate[100],
+    },
+  },
+}));
