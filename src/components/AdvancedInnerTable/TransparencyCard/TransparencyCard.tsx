60a745952f67cd130dc4aad661adf3a52ff83710
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-07-03 01:39:19 -0300

    Add style for the cards

--- src/components/AdvancedInnerTable/TransparencyCard/TransparencyCard.tsx
+++ src/components/AdvancedInnerTable/TransparencyCard/TransparencyCard.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material';
 import React from 'react';
-import type { CardSpacingSize } from '../types';
+import type { CardSpacingSize, ItemType } from '../types';
 
 interface Props {
   header: JSX.Element | string;
@@ -8,63 +8,78 @@ interface Props {
   items?: JSX.Element[];
   separators?: boolean[];
   footer?: JSX.Element | string;
-  hasIcon?: boolean;
   // TODO: Type this to avoid lower and uppercase error
-  itemType: string;
+  itemType: ItemType;
   cardSpacingSize?: CardSpacingSize;
+  subHeader: string;
+  showSubHeader: boolean;
 }
 
-export const TransparencyCard: React.FC<Props> = ({ cardSpacingSize = 'large', ...props }) => (
-  <Container
-    spacing={cardSpacingSize}
-    style={{ marginTop: props.itemType === 'total' ? 24 : 0 }}
-    className={`advance-table--transparency-card ${
-      props.itemType === 'total' ? 'advance-table--transparency-card_total' : 'advance-table--transparency_item'
-    }`}
-  >
-    <HeaderWrapper>{props.header}</HeaderWrapper>
-    {props.headers.map((header, i) => {
-      const titleReactComponent = (header as JSX.Element).props?.title || '';
-      const totalsStyle = header === 'Totals' || titleReactComponent === 'Totals';
+export const TransparencyCard: React.FC<Props> = ({
+  cardSpacingSize = 'large',
+  header,
+  headers,
+  itemType,
+  subHeader,
+  footer,
+  items,
+  separators,
+  showSubHeader,
+}) => {
+  if (itemType === 'section') return null;
+  return (
+    <Container
+      style={{ marginTop: itemType === 'total' ? 24 : 0 }}
+      className={`advance-table--transparency-card ${
+        itemType === 'total' ? 'advance-table--transparency-card_total' : 'advance-table--transparency_item'
+      }`}
+    >
+      <HeaderWrapper showSubHeader={showSubHeader}>
+        {header}
+        {showSubHeader && <SubHeader>{subHeader}</SubHeader>}
+      </HeaderWrapper>
+      {headers.map((header, i) => {
+        const titleReactComponent = (header as JSX.Element).props?.title || '';
+        const totalsStyle = header === 'Totals' || titleReactComponent === 'Totals';
 
-      return (
-        <>
-          <Row
-            key={header.toString()}
-            hasIcon={header !== 'Target Balance' || (header === 'Target Balance' && props.itemType === 'total')}
-          >
-            <Label hasIcon={header === 'Target Balance'} isTotal={totalsStyle}>
-              {header}
-            </Label>
-            <div
-              style={{
-                display: props.itemType === 'total' ? 'flex' : undefined,
-                justifyContent: props.itemType ? 'flex-end' : undefined,
-                width:
-                  header === 'Target Balance' || (header === 'Target Balance' && props.itemType !== 'total')
-                    ? '100%'
-                    : undefined,
-              }}
+        return (
+          <ContainerData spacing={cardSpacingSize}>
+            <Row
+              key={header.toString()}
+              hasIcon={header !== 'Target Balance' || (header === 'Target Balance' && itemType === 'total')}
             >
-              {(props.items && props.items[i]) ?? ''}
-            </div>
-          </Row>
-          {props.separators?.[i] && <ContainerLine />}
-        </>
-      );
-    })}
+              <Label hasIcon={header === 'Target Balance'} isTotal={totalsStyle}>
+                {header}
+              </Label>
+              <div
+                style={{
+                  display: itemType === 'total' ? 'flex' : undefined,
+                  justifyContent: itemType ? 'flex-end' : undefined,
+                  width:
+                    header === 'Target Balance' || (header === 'Target Balance' && itemType !== 'total')
+                      ? '100%'
+                      : undefined,
+                }}
+              >
+                {(items && items[i]) ?? ''}
+              </div>
+            </Row>
+            {separators?.[i] && <ContainerLine />}
+          </ContainerData>
+        );
+      })}
 
-    {props.footer && <FooterWrapper>{props.footer}</FooterWrapper>}
-  </Container>
-);
+      {footer && <FooterWrapper>{footer}</FooterWrapper>}
+    </Container>
+  );
+};
 
-const Container = styled('div')<{ spacing: CardSpacingSize }>(({ theme, spacing }) => ({
+const Container = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   boxShadow: theme.palette.isLight ? theme.fusionShadows.graphShadow : theme.fusionShadows.darkMode,
   background: theme.palette.isLight ? '#FFFFFF' : theme.palette.colors.charcoal[900],
-  padding: spacing === 'large' ? '20px 24px 10px' : '16px 16px 6px',
-  marginBottom: spacing === 'large' ? 24 : 8,
+  marginBottom: 24,
   borderRadius: '12px',
   [theme.breakpoints.between('mobile_375', 'tablet_768')]: {
     ':last-child': {
@@ -73,9 +88,15 @@ const Container = styled('div')<{ spacing: CardSpacingSize }>(({ theme, spacing 
   },
 }));
 
-const HeaderWrapper = styled('div')({
-  margin: '-16px 0 0 -16px',
-});
+const HeaderWrapper = styled('div')<{ showSubHeader: boolean }>(({ theme, showSubHeader }) => ({
+  backgroundColor: !showSubHeader
+    ? 'none'
+    : theme.palette.isLight
+    ? theme.palette.colors.slate[50]
+    : theme.palette.colors.charcoal[800],
+  borderTopLeftRadius: 12,
+  borderTopRightRadius: 12,
+}));
 
 const FooterWrapper = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -119,4 +140,21 @@ const ContainerLine = styled('div')(({ theme }) => ({
   borderTop: `1px solid ${theme.palette.isLight ? '#D4D9E1' : '#405361'}`,
   marginBottom: 20,
   marginTop: 20,
+}));
+
+const ContainerData = styled('div')<{ spacing: CardSpacingSize }>(({ spacing }) => ({
+  padding: spacing === 'large' ? '20px 24px 10px' : '16px 24px 6px',
+}));
+
+const SubHeader = styled('div')(({ theme }) => ({
+  color: theme.palette.isLight ? theme.palette.colors.gray[900] : theme.palette.colors.gray[50],
+  fontSize: 14,
+  fontWeight: 600,
+  lineHeight: '22px',
+  paddingLeft: 24,
+  paddingRight: 24,
+  paddingBottom: 8,
+  [theme.breakpoints.up('tablet_768')]: {
+    paddingLeft: 16,
+  },
 }));
