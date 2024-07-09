7efaf887b467315952143afead20ddcd000f5cbc
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-07-09 08:58:29 -0300

    Fix the padding in the links component

--- src/components/BudgetStatement/BudgetStatementForecast/BudgetStatementForecast.tsx
+++ src/components/BudgetStatement/BudgetStatementForecast/BudgetStatementForecast.tsx
@@ -1,11 +1,7 @@
 import { styled } from '@mui/material';
 import React from 'react';
-import { AdvancedInnerTable } from '@/components/AdvancedInnerTable/AdvancedInnerTable';
-import Tabs from '@/components/Tabs/Tabs';
-
-import { TransparencyEmptyTable } from '@/views/CoreUnitBudgetStatement/components/Placeholders/TransparencyEmptyTable';
-import { FORECAST_BREAKDOWN_QUERY_PARAM } from '@/views/CoreUnitBudgetStatement/utils/constants';
-import { BreakdownTableWrapper } from '../BudgetStatementActuals/BudgetStatementActuals';
+import ForecastBreakdownSection from './ForecastBreakdownSection/ForecastBreakdownSection';
+import TotalForecastSection from './TotalForecastSection/TotalForecastSection';
 import { useBudgetStatementForecast } from './useBudgetStatementForecast';
 import type { BudgetStatement } from '@ses/core/models/interfaces/budgetStatement';
 import type { ResourceType } from '@ses/core/models/interfaces/types';
@@ -41,47 +37,25 @@ export const BudgetStatementForecast: React.FC<BudgetStatementForecastProps> = (
   return (
     <Container>
       {headline}
-
-      <Title>{currentMonth.toFormat('MMM yyyy')} Totals</Title>
-      <AdvancedInnerTable
+      <TotalForecastSection
+        currentMonth={currentMonth}
         longCode={longCode}
-        columns={mainTableColumns}
-        items={mainTableItems}
-        style={{ marginBottom: '64px' }}
-        cardsTotalPosition={'top'}
-        tablePlaceholder={
-          <TransparencyEmptyTable breakdown longCode={longCode} shortCode={shortCode} resource={resource} />
-        }
+        mainTableColumns={mainTableColumns}
+        mainTableItems={mainTableItems}
+        shortCode={shortCode}
       />
-      {!!mainTableItems?.length && (
-        <Title marginBottom={24} ref={breakdownTitleRef}>
-          {currentMonth.toFormat('MMM yyyy')} Breakdown
-        </Title>
-      )}
-
-      {!!mainTableItems?.length && (
-        <Tabs
-          tabs={breakdownTabs.map((header, i) => ({
-            item: header,
-            id: headerIds[i],
-          }))}
-          tabQuery={FORECAST_BREAKDOWN_QUERY_PARAM}
-        />
-      )}
-
-      {!!mainTableItems?.length && (
-        <BreakdownTableWrapper>
-          <AdvancedInnerTable
-            longCode={longCode}
-            columns={breakdownColumnsForActiveTab}
-            items={breakdownItems}
-            cardSpacingSize="small"
-            tablePlaceholder={
-              <TransparencyEmptyTable breakdown longCode={longCode} shortCode={shortCode} resource={resource} />
-            }
-          />
-        </BreakdownTableWrapper>
-      )}
+      <ForecastBreakdownSection
+        breakdownColumnsForActiveTab={breakdownColumnsForActiveTab}
+        breakdownItems={breakdownItems}
+        breakdownTabs={breakdownTabs}
+        breakdownTitleRef={breakdownTitleRef}
+        currentMonth={currentMonth}
+        headerIds={headerIds}
+        longCode={longCode}
+        mainTableItems={mainTableItems}
+        resource={resource}
+        shortCode={shortCode}
+      />
     </Container>
   );
 };
@@ -91,31 +65,3 @@ const Container = styled('div')({
   flexDirection: 'column',
   marginBottom: 64,
 });
-
-const Title = styled('div')<{
-  marginBottom?: number;
-  fontSize?: string;
-  responsiveMarginBottom?: number;
-  isTitleOfPage?: boolean;
-  marginTop?: number;
-}>(({ marginBottom = 16, theme, responsiveMarginBottom, isTitleOfPage = false, marginTop = 24 }) => ({
-  fontFamily: 'Inter, sans-serif',
-  fontWeight: isTitleOfPage ? 500 : 600,
-  fontStyle: 'normal',
-  fontSize: 16,
-  lineHeight: '19px',
-  marginTop,
-  letterSpacing: '0.4px',
-  color: theme.palette.isLight ? theme.palette.colors.gray[900] : theme.palette.colors.gray[50],
-  marginBottom: `${marginBottom}px`,
-
-  [theme.breakpoints.up('tablet_768')]: {
-    fontSize: '18px',
-    lineHeight: '24px',
-    marginBottom: `${responsiveMarginBottom || marginBottom}px`,
-  },
-
-  [theme.breakpoints.between('mobile_375', 'tablet_768')]: {
-    fontWeight: 700,
-  },
-}));
---
+++ src/components/BudgetStatement/BudgetStatementForecast/TotalForecastSection/TotalForecastSection.tsx
@@ -0,0 +1,69 @@
+import { styled } from '@mui/material';
+import React from 'react';
+import { AdvancedInnerTable } from '@/components/AdvancedInnerTable/AdvancedInnerTable';
+import type { InnerTableColumn, InnerTableRow } from '@/components/AdvancedInnerTable/types';
+import BudgetStatementsPlaceholder from '@/components/PlaceHolders/BudgetStatementsPlaceholder';
+import type { ResourceType } from '@/core/models/interfaces/types';
+import type { DateTime } from 'luxon';
+import type { FC } from 'react';
+
+interface Props {
+  currentMonth: DateTime;
+  longCode: string;
+  shortCode: string;
+  mainTableColumns: InnerTableColumn[];
+  mainTableItems: InnerTableRow[];
+  resource?: ResourceType;
+}
+
+const TotalForecastSection: FC<Props> = ({
+  currentMonth,
+  longCode,
+  shortCode,
+  mainTableColumns,
+  mainTableItems,
+  resource,
+}) => (
+  <Container>
+    <Title>{currentMonth.toFormat('MMM yyyy')} Totals</Title>
+    <AdvancedInnerTable
+      longCode={longCode}
+      columns={mainTableColumns}
+      items={mainTableItems}
+      cardsTotalPosition={'top'}
+      tablePlaceholder={<BudgetStatementsPlaceholder longCode={longCode} shortCode={shortCode} resource={resource} />}
+    />
+  </Container>
+);
+
+export default TotalForecastSection;
+
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  marginBottom: 64,
+});
+const Title = styled('div')<{
+  marginBottom?: number;
+  fontSize?: string;
+  responsiveMarginBottom?: number;
+  isBreakDown?: boolean;
+  marginTop?: number;
+}>(({ marginBottom = 16, theme, responsiveMarginBottom, marginTop = 24, isBreakDown = false }) => ({
+  fontFamily: 'Inter, sans-serif',
+  fontWeight: isBreakDown ? 700 : 600,
+  fontStyle: 'normal',
+  fontSize: 16,
+  lineHeight: isBreakDown ? '19.36px' : '24px',
+  marginTop,
+  letterSpacing: '0.4px',
+  color: theme.palette.isLight ? theme.palette.colors.gray[900] : theme.palette.colors.gray[50],
+  marginBottom: `${marginBottom}px`,
+
+  [theme.breakpoints.up('tablet_768')]: {
+    fontSize: '18px',
+    lineHeight: '21.6px',
+    fontWeight: 700,
+    marginBottom: `${responsiveMarginBottom || marginBottom}px`,
+  },
+}));
