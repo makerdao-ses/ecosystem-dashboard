59701c0729b699789e2745540a865bc5c6ac00b0
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-07-08 22:05:07 -0300

    Rename the transparency to budgement

--- src/components/BudgetStatement/BudgetStatementActuals/BudgetStatementActuals.tsx
+++ src/components/BudgetStatement/BudgetStatementActuals/BudgetStatementActuals.tsx
@@ -9,7 +9,7 @@ import type { BudgetStatement } from '@ses/core/models/interfaces/budgetStatemen
 import type { ResourceType } from '@ses/core/models/interfaces/types';
 import type { DateTime } from 'luxon';
 
-interface TransparencyActualsProps {
+interface BudgetStatementActualsProps {
   currentMonth: DateTime;
   budgetStatements?: BudgetStatement[];
   longCode: string;
@@ -18,7 +18,7 @@ interface TransparencyActualsProps {
   resource: ResourceType;
 }
 
-export const BudgetStatementActuals: React.FC<TransparencyActualsProps> = ({
+export const BudgetStatementActuals: React.FC<BudgetStatementActualsProps> = ({
   currentMonth,
   budgetStatements,
   longCode,
@@ -46,17 +46,19 @@ export const BudgetStatementActuals: React.FC<TransparencyActualsProps> = ({
         shortCode={shortCode}
         resource={resource}
       />
-      <BreakdownSection
-        currentMonth={currentMonth}
-        mainTableItems={mainTableItems}
-        longCode={longCode}
-        shortCode={shortCode}
-        breakdownTitleRef={breakdownTitleRef}
-        breakdownTabs={breakdownTabs}
-        headerIds={headerIds}
-        breakdownColumnsForActiveTab={breakdownColumnsForActiveTab}
-        breakdownItemsForActiveTab={breakdownItemsForActiveTab}
-      />
+      <ContainerBreakdown>
+        <BreakdownSection
+          currentMonth={currentMonth}
+          mainTableItems={mainTableItems}
+          longCode={longCode}
+          shortCode={shortCode}
+          breakdownTitleRef={breakdownTitleRef}
+          breakdownTabs={breakdownTabs}
+          headerIds={headerIds}
+          breakdownColumnsForActiveTab={breakdownColumnsForActiveTab}
+          breakdownItemsForActiveTab={breakdownItemsForActiveTab}
+        />
+      </ContainerBreakdown>
       <CategoryModalComponent />
     </Container>
   );
@@ -89,27 +91,6 @@ export const BreakdownTableWrapper = styled('div')({
   paddingTop: 24,
 });
 
-// const Title = styled('div')<{
-//   marginBottom?: number;
-//   fontSize?: string;
-//   responsiveMarginBottom?: number;
-//   isBreakDown?: boolean;
-//   marginTop?: number;
-// }>(({ marginBottom = 16, theme, responsiveMarginBottom, marginTop = 24, isBreakDown = false }) => ({
-//   fontFamily: 'Inter, sans-serif',
-//   fontWeight: isBreakDown ? 700 : 600,
-//   fontStyle: 'normal',
-//   fontSize: 16,
-//   lineHeight: isBreakDown ? '19.36px' : '24px',
-//   marginTop,
-//   letterSpacing: '0.4px',
-//   color: theme.palette.isLight ? theme.palette.colors.gray[900] : theme.palette.colors.gray[50],
-//   marginBottom: `${marginBottom}px`,
-
-//   [theme.breakpoints.up('tablet_768')]: {
-//     fontSize: '18px',
-//     lineHeight: '21.6px',
-//     fontWeight: 700,
-//     marginBottom: `${responsiveMarginBottom || marginBottom}px`,
-//   },
-// }));
+const ContainerBreakdown = styled('div')({
+  marginTop: 8,
+});
