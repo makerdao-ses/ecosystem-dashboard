814d77429c1a477825d93848f87bf2ba99bdd7bb
Nestor <142045765+nesdc88@users.noreply.github.com>
2024-08-03 20:22:34 +0100

    Fusion/feat/hom 1.5 roadmap section (#1837)
    * feat & fix: HOM-1.5 Homepage Roadmap Section
    - Should change the content when the user changes to a different tab
    - Fix Header section display
    
    * fix: Types
    
    * feat: HOM-1.5 Homepage Roadmap Section
    - Should add milestones structure for mobile
    
    * feat: HOM-1.5 Homepage Roadmap Section
    - Should integrate the milestones with the API
    - Should add the milestones card header
    - Should add the description
    
    * fix: Chromatic error (empty roadmap data)
    
    * fix: Chromatic errors related with empty data in stories args
    
    * fix & improve: Stories args data (Home, RoadmapSection & MilestoneCard)
    
    * improve: Remove comments in stories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
+import type { Roadmap } from '@/core/models/interfaces/roadmaps';
 import type { Team } from '@/core/models/interfaces/team';
 import { fetchActors } from '@/views/EcosystemActorsIndex/api/queries';
 import HomeView from '@/views/Home/HomeView';
 import type { RevenueAndSpendingRecords } from '@/views/Home/api/queries';
 import { getRevenueAndSpendingData } from '@/views/Home/api/queries';
+import { getScopeOfWorkState } from '@/views/RoadmapMilestones/api/queries';
 import type { GetServerSideProps, NextPage } from 'next';
 
 interface HomePageProps {
   revenueAndSpendingData: RevenueAndSpendingRecords;
   teams: Team[];
+  roadmaps: Roadmap[];
 }
 
-const HomePage: NextPage<HomePageProps> = ({ revenueAndSpendingData, teams }) => (
-  <HomeView revenueAndSpendingData={revenueAndSpendingData} teams={teams} />
+const HomePage: NextPage<HomePageProps> = ({ revenueAndSpendingData, teams, roadmaps }) => (
+  <HomeView revenueAndSpendingData={revenueAndSpendingData} teams={teams} roadmaps={roadmaps} />
 );
 
 export default HomePage;
@@ -19,11 +22,13 @@ export default HomePage;
 export const getServerSideProps: GetServerSideProps = async () => {
   const revenueAndSpendingData = await getRevenueAndSpendingData();
   const teams = await fetchActors();
+  const roadmaps = await getScopeOfWorkState();
 
   return {
     props: {
       revenueAndSpendingData,
       teams,
+      roadmaps,
     },
   };
 };
