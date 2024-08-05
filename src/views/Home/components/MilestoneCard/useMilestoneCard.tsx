59517ffb62361b1f9926c6a1b59f601eea6de623
nesdc88 <nestor.diaz@dspot.com.pl>
2024-08-05 17:01:46 +0100

    feat: HOM-1.5 Homepage Roadmap Section - Should add a progress bars and status labels to the milestones card

--- src/views/Home/components/MilestoneCard/useMilestoneCard.tsx
+++ src/views/Home/components/MilestoneCard/useMilestoneCard.tsx
@@ -1,3 +1,27 @@
-const useMilestoneCard = () => null;
+import { useMemo } from 'react';
+
+import type { Maybe } from '@/core/models/interfaces/generics';
+import { DeliverableSetStatus } from '@/core/models/interfaces/roadmaps';
+
+const useMilestoneCard = (status: Maybe<DeliverableSetStatus>) => {
+  const statusLabel = useMemo(() => {
+    switch (status) {
+      case DeliverableSetStatus.FINISHED:
+        return 'Delivered';
+      case DeliverableSetStatus.IN_PROGRESS:
+        return 'In Progress';
+      case DeliverableSetStatus.DRAFT:
+        return 'Draft';
+      case DeliverableSetStatus.TODO:
+        return 'To do';
+      default:
+        return '';
+    }
+  }, [status]);
+
+  return {
+    statusLabel,
+  };
+};
 
 export default useMilestoneCard;
--- src/views/Home/components/RoadmapSection/RoadmapSection.stories.tsx
+++ src/views/Home/components/RoadmapSection/RoadmapSection.stories.tsx
@@ -1,4 +1,5 @@
 import { createThemeModeVariants } from '@ses/core/utils/storybook/factories';
+import { DefaultRoadmap } from '@/views/RoadmapMilestones/staticData';
 import RoadmapSection from './RoadmapSection';
 import type { Meta } from '@storybook/react';
 import type { FigmaParams } from 'sb-figma-comparator';
@@ -18,114 +19,7 @@ export default meta;
 
 const variantsArgs = [
   {
-    roadmaps: [
-      {
-        id: '1',
-        slug: 'ph-2024',
-        title: 'Powerhouse Roadmap 2024',
-        description: 'Powerhouse Ecosystem Actor team roadmap for the year 2024.',
-        milestones: [
-          {
-            id: 'ustpb52jla',
-            sequenceCode: 'PH01',
-            code: 'POC',
-            title: 'Decentralized Operations Platform - POC',
-            abstract: 'The initial phase of Powerhouse Decentralized Operations Platform.',
-            scope: {
-              deliverables: [
-                {
-                  id: 'oy69oibt04',
-                  code: 'POC1',
-                  title: 'First technical integration of RWA Portfolio (Connect & Switchboard)',
-                  description:
-                    'Technical integration demo showcasing for the first time the RWA Portfolio Editor in Connect and the data synchronization with Switchboard.',
-                  status: 'DELIVERED',
-                  keyResults: [
-                    {
-                      id: 'B84xO69g',
-                      title: 'RWA Conceptual Wireframes',
-                      link: 'https://drive.google.com/file/d/1NZXm_Q43sKH5pqwHTwN0DYvSW1uewMlY/view',
-                    },
-                    {
-                      id: '4965Bh97',
-                      title: 'First demo of RWA Portfolio - Feb 21',
-                      link: 'https://drive.google.com/file/d/1CMwePiR046IJqQGLypi7Fzu_B7aLYNco/view',
-                    },
-                  ],
-                  workProgress: {
-                    __typename: 'Percentage',
-                    value: 1,
-                  },
-                  budgetAnchor: {
-                    project: {
-                      code: 'RWA',
-                      title: 'RWA Portfolio Reporting',
-                    },
-                    workUnitBudget: 1,
-                    deliverableBudget: 0,
-                  },
-                  owner: {
-                    id: 'H7Cx8em3',
-                    ref: 'makerdao/ecosystem-actor',
-                    name: 'Powerhouse',
-                    code: 'PH',
-                    imageUrl:
-                      'https://makerdao-ses.github.io/ecosystem-dashboard/ecosystem-actors/POWERHOUSE/POWERHOUSE_logo.png',
-                  },
-                },
-              ],
-              status: 'DRAFT',
-              progress: {
-                __typename: 'Percentage',
-                value: 1,
-              },
-              totalDeliverables: 3,
-              deliverablesCompleted: 3,
-            },
-            coordinators: [
-              {
-                imageUrl: 'N/A',
-                code: 'Prometheus',
-                name: 'Prometheus',
-                ref: 'makerdao/contributor',
-                id: 'Z89231k5',
-              },
-              {
-                imageUrl: 'N/A',
-                code: 'cc00',
-                name: 'cc00',
-                ref: 'makerdao/contributor',
-                id: '66ICk94v',
-              },
-              {
-                imageUrl: 'N/A',
-                code: 'cc11',
-                name: 'cc11',
-                ref: 'makerdao/contributor',
-                id: '47z35Z8x',
-              },
-              {
-                imageUrl: 'N/A',
-                code: 'cc22',
-                name: 'cc22',
-                ref: 'makerdao/contributor',
-                id: '05SGYZ20',
-              },
-            ],
-            contributors: [
-              {
-                id: 'H7Cx8em3',
-                ref: 'makerdao/ecosystem-actor',
-                name: 'Powerhouse',
-                code: 'PH',
-                imageUrl:
-                  'https://makerdao-ses.github.io/ecosystem-dashboard/ecosystem-actors/POWERHOUSE/POWERHOUSE_logo.png',
-              },
-            ],
-          },
-        ],
-      },
-    ],
+    roadmaps: [{ ...DefaultRoadmap }],
   },
 ];
 
