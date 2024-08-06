cc605e80eface26a6f487c63e64c1ce94478393a
Yasiel Cabrera <yasiel9506@gmail.com>
2024-08-06 16:48:32 -0400

    feat: integrate right side chart with the API (WIP)

---
+++ src/views/Home/api/finances.ts
@@ -0,0 +1,108 @@
+import request, { gql } from 'graphql-request';
+import clone from 'lodash/cloneDeep';
+import { GRAPHQL_ENDPOINT } from '@/config/endpoints';
+import type { Analytic } from '@/core/models/interfaces/analytic';
+
+const query = gql`
+  query Analytics($filter: AnalyticsFilter) {
+    analytics {
+      series(filter: $filter) {
+        period
+        rows {
+          dimensions {
+            path
+          }
+          value
+          metric
+        }
+      }
+    }
+  }
+`;
+
+const filter = {
+  filter: {
+    start: '2021-01-01',
+    end: '2025-01-01',
+    granularity: 'quarterly',
+    metrics: ['Actuals', 'PaymentsOnChain', 'ProtocolNetOutflow', 'PaymentsOffChainIncluded', 'Forecast'],
+    dimensions: [
+      {
+        name: 'budget',
+        select: 'atlas',
+        lod: 4,
+      },
+    ],
+    currency: 'DAI',
+  },
+};
+
+export type MetricKey = 'Actuals' | 'PaymentsOnChain' | 'ProtocolNetOutflow' | 'PaymentsOffChainIncluded' | 'Forecast';
+export type BudgetKey =
+  | 'legacyOthers'
+  | 'legacyCoreUnits'
+  | 'governanceScope'
+  | 'stability'
+  | 'support'
+  | 'protocol'
+  | 'accessibility'
+  | 'immutable';
+export type FormattedFinancesData = Record<MetricKey, Record<BudgetKey, number[]>>;
+
+const pathRegexMap: Record<BudgetKey, RegExp> = {
+  legacyOthers: /^atlas\/legacy\/(?!.*core-units).*$/g,
+  legacyCoreUnits: /^atlas\/legacy\/core-units.*$/g,
+  governanceScope: /^atlas\/scopes\/GOV.*$/g,
+  stability: /^atlas\/scopes\/STA.*$/g,
+  support: /^atlas\/scopes\/SUP.*$/g,
+  protocol: /^atlas\/scopes\/PRO.*$/g,
+  accessibility: /^atlas\/scopes\/ACC.*$/g,
+  immutable: /^atlas\/immutable.*$/g,
+};
+
+export const getFinancesData = async (): Promise<FormattedFinancesData> => {
+  const analyticsResponse = await request<{
+    analytics: Analytic;
+  }>(GRAPHQL_ENDPOINT, query, filter);
+
+  const emptyData: Record<BudgetKey, number[]> = {
+    legacyOthers: [] as number[],
+    legacyCoreUnits: [] as number[],
+    governanceScope: [] as number[],
+    stability: [] as number[],
+    support: [] as number[],
+    protocol: [] as number[],
+    accessibility: [] as number[],
+    immutable: [] as number[],
+  };
+
+  const data: Record<MetricKey, typeof emptyData> = {
+    Actuals: clone(emptyData),
+    PaymentsOnChain: clone(emptyData),
+    ProtocolNetOutflow: clone(emptyData),
+    PaymentsOffChainIncluded: clone(emptyData),
+    Forecast: clone(emptyData),
+  };
+
+  analyticsResponse.analytics.series.forEach((series) => {
+    // quarter x
+    const year = Number(series.period.split('/')[0]);
+    const quarter = Number(series.period.charAt(6));
+    series.rows.forEach((row) => {
+      const metric = row.metric as MetricKey;
+      Object.keys(pathRegexMap).forEach((key) => {
+        if (pathRegexMap?.[key as BudgetKey].test(row.dimensions[0].path)) {
+          const index = (year - 2021) * 4 + quarter - 1;
+
+          if (!data[metric][key as BudgetKey][index]) {
+            data[metric][key as BudgetKey][index] = row.value;
+          } else {
+            data[metric][key as BudgetKey][index] += row.value;
+          }
+        }
+      });
+    });
+  });
+
+  return data;
+};
--- src/views/Home/components/FinancesLineChart/FinancesLineChart.tsx
+++ src/views/Home/components/FinancesLineChart/FinancesLineChart.tsx
@@ -1,15 +1,22 @@
 import { styled, useMediaQuery, useTheme } from '@mui/material';
 import ReactECharts from 'echarts-for-react';
+import { replaceAllNumberLetOneBeforeDot } from '@/core/utils/string';
 import useFinancesLineChart from './useFinancesLineChart';
+import type { FormattedFinancesData, MetricKey } from '../../api/finances';
 import type { Theme } from '@mui/material';
 import type { EChartsOption } from 'echarts-for-react';
 import type { FC } from 'react';
 
-const FinancesLineChart: FC = () => {
+interface FinancesLineChartProps {
+  financesData: FormattedFinancesData;
+  selectedMetric: MetricKey;
+}
+
+const FinancesLineChart: FC<FinancesLineChartProps> = ({ financesData, selectedMetric }) => {
   const { financesLineChartRef } = useFinancesLineChart();
   const theme = useTheme();
-
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('tablet_768'));
+  console.log(financesData);
 
   const series = [
     {
@@ -22,7 +29,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [260, 220, 190, 160, 120, 80, 50],
+      // data: [260, 220, 190, 160, 120, 80, 50],
+      data: financesData[selectedMetric].legacyOthers,
       itemStyle: {
         color: theme.palette.colors.charcoal[300],
       },
@@ -37,7 +45,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [480, 400, 300, 200, 140, 100, 40],
+      // data: [480, 400, 300, 200, 140, 100, 40],
+      data: financesData[selectedMetric].legacyCoreUnits,
       itemStyle: {
         color: theme.palette.colors.charcoal[200],
       },
@@ -52,7 +61,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 10, 80, 160, 200, 200, 140, 100, 50, 50, 50],
+      // data: [null, null, null, null, null, null, 10, 80, 160, 200, 200, 140, 100, 50, 50, 50],
+      data: financesData[selectedMetric].governanceScope,
       itemStyle: {
         color: theme.palette.colors.fusion[400],
       },
@@ -67,7 +77,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 10, 80, 160, 100, 120, 140, 100, 50, 50, 50],
+      // data: [null, null, null, null, null, null, 10, 80, 160, 100, 120, 140, 100, 50, 50, 50],
+      data: financesData[selectedMetric].stability,
       itemStyle: {
         color: theme.palette.colors.blue[500],
       },
@@ -82,7 +93,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 10, 80, 160, 150, 160, 140, 100, 50, 50, 50],
+      // data: [null, null, null, null, null, null, 10, 80, 160, 150, 160, 140, 100, 50, 50, 50],
+      data: financesData[selectedMetric].support,
       itemStyle: {
         color: theme.palette.colors.red[500],
       },
@@ -97,7 +109,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 10, 80, 160, 150, 160, 140, 100, 50, 50, 50],
+      // data: [null, null, null, null, null, null, 10, 80, 160, 150, 160, 140, 100, 50, 50, 50],
+      data: financesData[selectedMetric].protocol,
       itemStyle: {
         color: theme.palette.colors.green[500],
       },
@@ -112,7 +125,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 20, 80, 160, 150, 160, 140, 100, 50, 50, 80],
+      // data: [null, null, null, null, null, null, 20, 80, 160, 150, 160, 140, 100, 50, 50, 80],
+      data: financesData[selectedMetric].accessibility,
       itemStyle: {
         color: theme.palette.colors.purple[500],
       },
@@ -127,7 +141,8 @@ const FinancesLineChart: FC = () => {
         focus: 'series',
       },
       showSymbol: false,
-      data: [null, null, null, null, null, null, 30, 80, 160, 150, 160, 140, 100, 180, 50, 100],
+      // data: [null, null, null, null, null, null, 30, 80, 160, 150, 160, 140, 100, 180, 50, 100],
+      data: financesData[selectedMetric].immutable,
       itemStyle: {
         color: theme.palette.colors.orange[500],
       },
@@ -180,7 +195,13 @@ const FinancesLineChart: FC = () => {
         fontSize: isMobile ? 12 : 14,
         lineHeight: isMobile ? 16 : 19,
         color: theme.palette.isLight ? theme.palette.colors.gray[600] : theme.palette.colors.gray[500],
-        formatter: (value: number) => `${value} K`,
+        formatter: (value: number, index: number) => {
+          if (value === 0 && index === 0) {
+            return value.toString();
+          }
+
+          return replaceAllNumberLetOneBeforeDot(value, true);
+        },
       },
     },
     series,
